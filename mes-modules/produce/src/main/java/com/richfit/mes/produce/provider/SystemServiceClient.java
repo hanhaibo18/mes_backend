4b02af134a5963514bfcbe2d1505a596cd10a1ef
mafeng02 <mafeng02@cnpc.com.cn>
2022-01-13 17:29:06 +0800

    Update SystemServiceClient.java

--- mes-modules/produce/src/main/java/com/richfit/mes/produce/provider/SystemServiceClient.java
+++ mes-modules/produce/src/main/java/com/richfit/mes/produce/provider/SystemServiceClient.java
@@ -7,6 +7,9 @@ import com.richfit.mes.produce.provider.fallback.SystemServiceClientFallbackImpl
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import com.richfit.mes.common.model.sys.ItemClass;
+import com.richfit.mes.common.model.sys.ItemParam;
+import java.util.List;
 
 /**
  * @Author: GaoLiang
@@ -19,4 +22,7 @@ public interface SystemServiceClient {
     @GetMapping(value = "/api/sys/user/find_one")
     public CommonResult<TenantUserVo> getUserById(@RequestParam("id") String id);
 
+    @GetMapping(value = "/api/sys/item/item/param/list")
+    public CommonResult<List<ItemParam>> selectItemClass(@RequestParam("code") String code,@RequestParam("name") String name);
+    
 }
