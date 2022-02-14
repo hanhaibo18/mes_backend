f9183504d01c5aef00c13194841c6d395e3d2c13
mafeng02 <mafeng02@cnpc.com.cn>
2022-02-14 16:51:03 +0800

    Update SystemServiceClient.java

--- mes-modules/produce/src/main/java/com/richfit/mes/produce/provider/SystemServiceClient.java
+++ mes-modules/produce/src/main/java/com/richfit/mes/produce/provider/SystemServiceClient.java
@@ -1,16 +1,20 @@
 package com.richfit.mes.produce.provider;
 
 import com.richfit.mes.common.core.api.CommonResult;
+import com.richfit.mes.common.model.sys.Attachment;
 import com.richfit.mes.common.model.sys.TenantUser;
 import com.richfit.mes.common.model.sys.vo.TenantUserVo;
 import com.richfit.mes.common.security.constant.SecurityConstants;
 import com.richfit.mes.produce.provider.fallback.SystemServiceClientFallbackImpl;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import com.richfit.mes.common.model.sys.ItemClass;
 import com.richfit.mes.common.model.sys.ItemParam;
+
+import java.io.InputStream;
 import java.util.List;
 
 /**
@@ -26,5 +30,12 @@ public interface SystemServiceClient {
 
     @GetMapping(value = "/api/sys/item/item/param/list" )
     public CommonResult<List<ItemParam>> selectItemClass(@RequestParam("code") String code,@RequestParam("name") String name,@RequestHeader(value = SecurityConstants.FROM)String header);
+
+    @GetMapping(value = "/api/sys/attachment/get/{id}")
+    public CommonResult<Attachment> attachment(@PathVariable String id);
+
+    @GetMapping(value = "/api/sys/attachment/getinput/{id}")
+    public CommonResult<byte[]> getAttachmentInputStream(@PathVariable String id);
+
 
 }
