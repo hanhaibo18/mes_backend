e968ad1eb47c2775dcbc5a22051710d5b036c504
mafeng02 <mafeng02@cnpc.com.cn>
2022-03-02 16:51:25 +0800

    Update TrackCompleteServiceImpl.java

--- mes-modules/produce/src/main/java/com/richfit/mes/produce/service/TrackCompleteServiceImpl.java
+++ mes-modules/produce/src/main/java/com/richfit/mes/produce/service/TrackCompleteServiceImpl.java
@@ -1,6 +1,10 @@
 package com.richfit.mes.produce.service;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.richfit.mes.common.model.produce.Assign;
 import com.richfit.mes.common.model.produce.TrackComplete;
 import com.richfit.mes.produce.dao.TrackCompleteMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,4 +20,9 @@ public class TrackCompleteServiceImpl extends ServiceImpl<TrackCompleteMapper, T
     @Autowired
     private TrackCompleteMapper trackCompleteMapper;
 
+    public IPage<TrackComplete> queryPage(Page page, QueryWrapper<TrackComplete> query)
+    {
+
+        return trackCompleteMapper.queryPage(page,query);
+    }
 }
