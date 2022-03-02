2a0991ffceec0326f87af80add7b539f8c7fc079
mafeng02 <mafeng02@cnpc.com.cn>
2022-03-02 16:50:30 +0800

    Update TrackCompleteMapper.java

--- mes-modules/produce/src/main/java/com/richfit/mes/produce/dao/TrackCompleteMapper.java
+++ mes-modules/produce/src/main/java/com/richfit/mes/produce/dao/TrackCompleteMapper.java
@@ -1,10 +1,12 @@
 package com.richfit.mes.produce.dao;
 
 import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.core.toolkit.Constants;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.richfit.mes.common.model.produce.Assign;
 import com.richfit.mes.common.model.produce.TrackComplete;
 import com.richfit.mes.common.model.produce.TrackItem;
 import org.apache.ibatis.annotations.Mapper;
@@ -17,4 +19,8 @@ import org.apache.ibatis.annotations.Select;
  */
 @Mapper
 public interface TrackCompleteMapper extends BaseMapper<TrackComplete> {
-   }
+
+   @Select("select * from v_produce_complete a ${ew.customSqlSegment}")
+   IPage<TrackComplete> queryPage(Page page, @Param(Constants.WRAPPER) Wrapper<TrackComplete> wrapper);
+
+}
