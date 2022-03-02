1f6900385c969c7f60d2c257baf1f51c45651352
mafeng02 <mafeng02@cnpc.com.cn>
2022-03-02 16:51:08 +0800

    Update TrackCompleteService.java

--- mes-modules/produce/src/main/java/com/richfit/mes/produce/service/TrackCompleteService.java
+++ mes-modules/produce/src/main/java/com/richfit/mes/produce/service/TrackCompleteService.java
@@ -1,6 +1,10 @@
 package com.richfit.mes.produce.service;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.richfit.mes.common.model.produce.Assign;
 import com.richfit.mes.common.model.produce.TrackComplete;
 
 /**
@@ -8,4 +12,7 @@ import com.richfit.mes.common.model.produce.TrackComplete;
  * @Description 报工服务
  */
 public interface TrackCompleteService extends IService<TrackComplete> {
+    IPage<TrackComplete> queryPage(Page page, QueryWrapper<TrackComplete> query);
+
+
 }
