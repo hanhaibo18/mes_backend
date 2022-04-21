139ec3188ad80b4bf8fff8d4138cf058ddec4ecf
houxinyu <houxinyu@cnpc.com.cn>
2022-04-21 17:59:46 +0800

    # 质检审核  - 增加根据图号和数据组查询工艺文档  - 根据图号和数据组查询工装

--- mes-modules/base/src/main/java/com/richfit/mes/base/service/PdmDrawService.java
+++ mes-modules/base/src/main/java/com/richfit/mes/base/service/PdmDrawService.java
@@ -3,6 +3,18 @@ package com.richfit.mes.base.service;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.richfit.mes.common.model.base.PdmDraw;
 
+import java.util.List;
+
 
 public interface PdmDrawService extends IService<PdmDraw> {
+
+    /**
+     * 功能描述: 根据图号查询工艺图纸
+     * @Author: xinYu.hou
+     * @Date: 2022/4/21 15:09
+     * @param itemId
+     * @param dataGroup
+     * @return: List<PdmDraw>
+     **/
+    List<PdmDraw> queryDraw(String itemId,String dataGroup);
 }
--- mes-modules/base/src/main/java/com/richfit/mes/base/service/PdmDrawServiceImpl.java
+++ mes-modules/base/src/main/java/com/richfit/mes/base/service/PdmDrawServiceImpl.java
@@ -1,9 +1,12 @@
 package com.richfit.mes.base.service;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.richfit.mes.base.dao.PdmDrawMapper;
 import com.richfit.mes.common.model.base.PdmDraw;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * @author rzw
@@ -11,4 +14,11 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class PdmDrawServiceImpl extends ServiceImpl<PdmDrawMapper, PdmDraw> implements PdmDrawService {
+    @Override
+    public List<PdmDraw> queryDraw(String itemId, String dataGroup) {
+        QueryWrapper<PdmDraw> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("item_id",itemId);
+        queryWrapper.eq("dataGroup",dataGroup);
+        return this.list(queryWrapper);
+    }
 }
