479f899d9fd2e25098f69264bcbb42791cf67091
gaoliang01 <gaoliang01@cnpc.com.cn>
2022-04-29 14:49:03 +0800

    增加字段swagger注解

--- mes-common/common-model/src/main/java/com/richfit/mes/common/model/base/Product.java
+++ mes-common/common-model/src/main/java/com/richfit/mes/common/model/base/Product.java
@@ -2,6 +2,7 @@ package com.richfit.mes.common.model.base;
 
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.richfit.mes.common.core.base.BaseEntity;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.util.Date;
@@ -9,6 +10,8 @@ import java.util.Date;
 /**
  * @author 王瑞
  * @Description 物料
+ *
+ * 20220429 gl 增加API注解，方便前端阅读接口文件
  */
 
 @Data
@@ -17,71 +20,85 @@ public class Product extends BaseEntity<Product> {
     /**
      * 租户ID
      */
+    @ApiModelProperty(value = "所属租户")
     private String tenantId;
 
     /**
      * 物料编码
      */
+    @ApiModelProperty(value = "物料编码")
     private String materialNo;
 
     /**
      * 物料日期
      */
+    @ApiModelProperty(value = "物料日期")
     private Date materialDate;
 
     /**
      * 物料类型 0铸件 1锻件 2精铸件 3成品/半成品
      */
+    @ApiModelProperty(value = "物料类型 0铸件 1锻件 2精铸件 3成品/半成品")
     private String materialType;
 
     /**
      * 物料描述
      */
+    @ApiModelProperty(value = "物料描述")
     private String materialDesc;
 
     /**
      * 图号
      */
+    @ApiModelProperty(value = "图号")
     private String drawingNo;
 
     /**
      * 产品名称
      */
+    @ApiModelProperty(value = "产品名称")
     private String productName;
 
     /**
      * 材质
      */
+    @ApiModelProperty(value = "材质")
     private String texture;
 
     /**
      * 重量
      */
+    @ApiModelProperty(value = "重量")
     private Float weight;
 
     /**
      * 单位
      */
+    @ApiModelProperty(value = "单位")
     private String unit;
 
     /**
      * 换算比例
      */
+    @ApiModelProperty(value = "换算比例")
     private Float convertScale;
 
     /**
      * 换算单位
      */
+    @ApiModelProperty(value = "换算单位")
     private String convertUnit;
 
     /**
      * 组织结构编码
      */
+    @ApiModelProperty(value = "组织结构编码")
     private String branchCode;
 
     /**
      * 类型 0：自制 1：外购 2：外协
      */
+    @ApiModelProperty(value = "类型 0：自制 1：外购 2：外协")
     private String objectType;
 
     @TableField(exist = false)
