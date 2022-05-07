e44bc4c27d90658ca5858899c45b58cd80eed108
gaoliang01 <gaoliang01@cnpc.com.cn>
2022-05-07 16:18:38 +0800

    工艺分页查询，同时返回历史工艺

--- mes-modules/base/src/main/java/com/richfit/mes/base/service/RouterService.java
+++ mes-modules/base/src/main/java/com/richfit/mes/base/service/RouterService.java
@@ -1,16 +1,18 @@
 package com.richfit.mes.base.service;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.richfit.mes.common.model.base.Router;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.richfit.mes.common.model.base.Router;
 
 /**
  * @author 马峰
  * @Description 工艺服务
  */
 public interface RouterService extends IService<Router> {
-    
+
     public IPage<Router> selectPage(Page page, QueryWrapper<Router> qw);
+
+    public IPage<Router> selectPageAndChind(Page page, QueryWrapper<Router> qw);
 }
