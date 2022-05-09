7ff2af7887a0799e038452eb6c71c4ee16d206d8
houxinyu <houxinyu@cnpc.com.cn>
2022-05-09 09:00:19 +0800

    # 站内短信  - 修改未选择收件人信息消息提示

--- mes-modules/system/src/main/java/com/richfit/mes/sys/controller/NoteController.java
+++ mes-modules/system/src/main/java/com/richfit/mes/sys/controller/NoteController.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.richfit.mes.common.core.api.CommonResult;
+import com.richfit.mes.common.core.api.IErrorCode;
 import com.richfit.mes.common.model.sys.Note;
 import com.richfit.mes.common.model.sys.NoteUser;
 import com.richfit.mes.common.model.sys.dto.NoteDto;
@@ -16,6 +17,7 @@ import com.richfit.mes.sys.service.NoteUserService;
 import io.swagger.annotations.Api;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+
 import javax.annotation.Resource;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -39,15 +41,17 @@ public class NoteController {
 
     @Resource
     private ObjectMapper objectMapper;
+
     /**
      * 功能描述: 删除收到的信息
+     *
+     * @param list
      * @Author: xinYu.hou
      * @Date: 2022/2/16 15:29
-     * @param list
      * @return: CommonResult<Boolean>
      **/
     @PutMapping("/delete/recipients")
-    public CommonResult<Boolean> deleteRecipients(@RequestBody List<NoteUserVo> list){
+    public CommonResult<Boolean> deleteRecipients(@RequestBody List<NoteUserVo> list) {
         Boolean recipients = false;
         for (NoteUserVo noteUserVo : list) {
             recipients = noteService.deleteRecipients(noteUserVo.getId());
@@ -57,16 +61,17 @@ public class NoteController {
 
     /**
      * 功能描述: 删除发送邮件
+     *
+     * @param idList
      * @Author: xinYu.hou
      * @Date: 2022/2/16 15:30
-     * @param idList
      * @return: CommonResult<Boolean>
      **/
     @PutMapping("/delete/sender")
     @Transactional(rollbackFor = Exception.class)
-    public CommonResult<Boolean> deleteSender(@RequestBody List<NoteVo> idList){
+    public CommonResult<Boolean> deleteSender(@RequestBody List<NoteVo> idList) {
         Boolean sender = false;
-        for(NoteVo noteVo : idList){
+        for (NoteVo noteVo : idList) {
             sender = noteService.deleteSender(noteVo);
         }
         return CommonResult.success(sender);
@@ -74,18 +79,19 @@ public class NoteController {
 
     /**
      * 功能描述: 发送个人短信
+     *
+     * @param
      * @Author:
      * @Date:
-     * @param
      * @return:
      **/
-    @PostMapping ("/save")
+    @PostMapping("/save")
     public CommonResult<Boolean> save(@RequestParam("sendUser") String sendUser,
-                                      @RequestParam("sendTitle") String  sendTitle,
-                                      @RequestParam("sendContent") String  sendContent,
-                                      @RequestParam("reseiverUsers") String  reseiverUsers,
-                                      @RequestParam("branchCode") String  branchCode,
-                                      @RequestParam("tenantId") String  tenantId){
+                                      @RequestParam("sendTitle") String sendTitle,
+                                      @RequestParam("sendContent") String sendContent,
+                                      @RequestParam("reseiverUsers") String reseiverUsers,
+                                      @RequestParam("branchCode") String branchCode,
+                                      @RequestParam("tenantId") String tenantId) {
         NoteDto noteDto = new NoteDto();
         noteDto.setContent(sendContent);
         noteDto.setTitle(sendTitle);
@@ -98,39 +104,42 @@ public class NoteController {
 
     /**
      * 功能描述: 查询收到的邮件
+     *
+     * @param queryDto
      * @Author: xinYu.hou
      * @Date: 2022/2/15 16:05
-     * @param queryDto
-     * @return: CommonResult<IPage<NoteVo>>
+     * @return: CommonResult<IPage < NoteVo>>
      **/
     @GetMapping("/query/recipients_page")
-    public CommonResult<IPage<NoteUserVo>> queryRecipients(QueryDto<String> queryDto){
+    public CommonResult<IPage<NoteUserVo>> queryRecipients(QueryDto<String> queryDto) {
         return noteService.queryRecipients(queryDto);
     }
 
     /**
      * 功能描述: 查询发送出去的信息
+     *
+     * @param queryDto
      * @Author: xinYu.hou
      * @Date: 2022/2/16 10:47
-     * @param queryDto
-     * @return: CommonResult<IPage<NoteVo>>
+     * @return: CommonResult<IPage < NoteVo>>
      **/
     @GetMapping("/query/sender_page")
-    public CommonResult<IPage<NoteVo>> querySender(QueryDto<String> queryDto){
+    public CommonResult<IPage<NoteVo>> querySender(QueryDto<String> queryDto) {
         return noteService.querySender(queryDto);
     }
 
     /**
      * 功能描述: 根据用户Id获取未读数量
+     *
+     * @param userId
      * @Author: xinYu.hou
      * @Date: 2022/3/4 14:49
-     * @param userId
      * @return: CommonResult<Integer>
      **/
     @GetMapping("/query/message_number/{userId}")
-    public CommonResult<Integer> queryMessageNumber(@PathVariable String userId){
+    public CommonResult<Integer> queryMessageNumber(@PathVariable String userId) {
         QueryWrapper<NoteUser> queryWrapper = new QueryWrapper<>();
-        queryWrapper.eq("user_account",userId);
+        queryWrapper.eq("user_account", userId);
         queryWrapper.eq("state", SenderEnum.NOT_READ.getStateId());
         int count = noteUserService.count(queryWrapper);
         return CommonResult.success(count);
@@ -138,13 +147,14 @@ public class NoteController {
 
     /**
      * 功能描述: 已读
+     *
+     * @param idList
      * @Author: xinYu.hou
      * @Date: 2022/3/8 16:53
-     * @param idList
      * @return: CommonResult<Boolean>
      **/
     @PutMapping("/read")
-    public CommonResult<Boolean> read(@RequestBody List<NoteUserVo> idList){
+    public CommonResult<Boolean> read(@RequestBody List<NoteUserVo> idList) {
         boolean update = false;
         for (NoteUserVo noteUserVo : idList) {
             NoteUser noteUser = new NoteUser();
@@ -157,37 +167,49 @@ public class NoteController {
 
     /**
      * 功能描述: 页面创建信息
+     *
+     * @param noteDto
      * @Author: xinYu.hou
      * @Date: 2022/3/11 17:27
-     * @param noteDto
      * @return: CommonResult<Boolean>
      **/
-    @PostMapping ("/save_page")
-    public CommonResult<Boolean> save(@RequestBody NoteDto noteDto){
+    @PostMapping("/save_page")
+    public CommonResult<Boolean> save(@RequestBody NoteDto noteDto) {
         boolean saveNote = noteService.saveNote(noteDto);
         if (saveNote) {
             CommonResult.success(true);
         }
-        return CommonResult.success(false,"请选择收件人");
+        return CommonResult.failed(new IErrorCode() {
+            @Override
+            public long getCode() {
+                return 500;
+            }
+
+            @Override
+            public String getMessage() {
+                return "请选择收件人";
+            }
+        });
     }
 
     /**
      * 功能描述: 查询个详情
-     * @Author: xinYu.hou
-     * @Date: 2022/3/14 10:14
+     *
      * @param id
      * @param userId
+     * @Author: xinYu.hou
+     * @Date: 2022/3/14 10:14
      * @return: CommonResult<Note>
      **/
     @GetMapping("/query_one/{id}/{userId}")
-    public CommonResult<Note> queryById(@PathVariable String id,@PathVariable String userId){
+    public CommonResult<Note> queryById(@PathVariable String id, @PathVariable String userId) {
         return noteService.queryById(id, userId);
     }
 
     @GetMapping("/query_one/note/{id}")
-    public CommonResult<Note> queryNote(@PathVariable String id){
+    public CommonResult<Note> queryNote(@PathVariable String id) {
         QueryWrapper<NoteUser> queryWrapper = new QueryWrapper<>();
-        queryWrapper.eq("note_id",id);
+        queryWrapper.eq("note_id", id);
         List<NoteUser> noteUserList = noteUserService.list(queryWrapper);
         List<String> tableNames = noteUserList.stream().map(NoteUser::getUserAccount).collect(Collectors.toList());
         return CommonResult.success(noteService.getById(id).setUserAccountList(tableNames));
