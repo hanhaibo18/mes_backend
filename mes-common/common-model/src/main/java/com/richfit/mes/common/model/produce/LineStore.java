5132831deac70458aced69f5d5fb8197253e38e2
gaoliang01 <gaoliang01@cnpc.com.cn>
2022-05-31 17:12:40 +0800

    doc: 完善注解

--- mes-common/common-model/src/main/java/com/richfit/mes/common/model/produce/LineStore.java
+++ mes-common/common-model/src/main/java/com/richfit/mes/common/model/produce/LineStore.java
@@ -2,6 +2,7 @@ package com.richfit.mes.common.model.produce;
 
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.richfit.mes.common.core.base.BaseEntity;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.util.Date;
@@ -13,182 +14,134 @@ import java.util.Date;
 @Data
 public class LineStore extends BaseEntity<LineStore> {
 
-    /**
-     * 租户ID
-     */
+    @ApiModelProperty(value = "租户ID")
     private String tenantId;
 
+    @ApiModelProperty(value = "计划编号")
     private String trackNo;
 
-    /**
-     * 物料编号
-     */
+    @ApiModelProperty(value = "物料编号")
     private String materialNo;
 
-    /**
-     * 图号
-     */
+    @ApiModelProperty(value = "图号")
     private String drawingNo;
 
-    /**
-     * 状态 0在制 1完工 2作废 3已消耗
-     */
+    @ApiModelProperty(value = "状态 0在制 1完工 2作废 3已消耗")
     private String status;
 
-    /**
-     * 数量
-     */
+    @ApiModelProperty(value = "数量")
     private Integer number;
 
-    /**
-     * 合格证编号
-     */
+    @ApiModelProperty(value = "合格证编号")
     private String certificateNo;
 
-    /**
-     * 物料来源
-     */
+    @ApiModelProperty(value = "物料来源")
     private String materialSource;
 
-    /**
-     * 炉批号
-     */
+    @ApiModelProperty(value = "炉批号")
     private String batchNo;
 
-    /**
-     * 工作号
-     */
+    @ApiModelProperty(value = "工作号")
     private String workNo;
 
-    /**
-     * 毛坯编号
-     */
+    @ApiModelProperty(value = "毛坯编号")
     private String workblankNo;
 
-    /**
-     * 物料类型 0毛坯 1半成品/成品
-     */
+    @ApiModelProperty(value = "物料类型 0毛坯 1半成品/成品")
     private String materialType;
 
-    /**
-     * 跟踪方式 0单件 1批次
-     */
+    @ApiModelProperty(value = "跟踪方式 0单件 1批次")
     private String trackType;
 
-    /**
-     * 已使用数量
-     */
+    @ApiModelProperty(value = "已使用数量")
     private Integer userNum;
 
+    @ApiModelProperty(value = "")
     @TableField(exist = false)
     private String assemblyId;
 
-    /**
-     * 入库时间
-     */
+    @ApiModelProperty(value = "入库时间")
     private Date inTime;
 
-    /**
-     * 出库时间
-     */
+    @ApiModelProperty(value = "出库时间")
     private Date outTime;
 
-    /**
-     * 试棒数量
-     */
+    @ApiModelProperty(value = "试棒数量")
     private Integer testBarNumber;
 
-    /**
-     * 试棒类型
-     */
+    @ApiModelProperty(value = "试棒类型")
     private String testBarType;
-    /**
-     * 生产订单编号
-     */
+
+    @ApiModelProperty(value = "生产订单编号")
     private String productionOrder;
-    /**
-     * 采购订单编号
-     */
+
+    @ApiModelProperty(value = "采购订单编号")
     private String purchaseOrder;
-    /**
-     * 物料描述
-     */
+
+    @ApiModelProperty(value = "物料描述")
     private String materialDesc;
+
+    @ApiModelProperty(value = "物料名称")
     private String materialName;
+
+    @ApiModelProperty(value = "产品名称")
     private String productName;
+
+    @ApiModelProperty(value = "材质")
     private String texture;
+
+    @ApiModelProperty(value = "重量")
     private Float weight;
-    /**
-     * 合同编号
-     */
+
+    @ApiModelProperty(value = "合同编号")
     private String contractNo;
-    /**
-     * 代用材料
-     */
+
+    @ApiModelProperty(value = "代用材料")
     private String replaceMaterial;
-    /**
-     * 预先派工
-     */
+
+    @ApiModelProperty(value = "预先派工")
     private String beforehandAssigned;
-    /**
-     * 上工序跟单编号
-     */
+
+    @ApiModelProperty(value = "上工序跟单编号")
     private String prevTrackNum;
-    /**
-     * 是否上传ERP
-     */
+
+    @ApiModelProperty(value = "是否上传ERP")
     private String isSendErp;
-    /**
-     * 是否投料ERP
-     */
+
+    @ApiModelProperty(value = "是否投料ERP")
     private String isFeedErp;
-    /**
-     * 料单类型  0 常规  1 自动
-     */
+
+    @ApiModelProperty(value = "料单类型  0 常规  1 自动")
     private String stockType;
 
-    /**
-     * 图号 产品编号
-     */
+    @ApiModelProperty(value = "产品编号")
     private String prodNo;
 
+    @ApiModelProperty(value = "所属分厂")
     private String branchCode;
 
-    /**
-     * 合格证文件ID
-     */
+    @ApiModelProperty(value = "合格证文件ID")
     private String certificateId;
 
-    /**
-     * 合格证备注
-     */
+    @ApiModelProperty(value = "合格证备注")
     private String certificateRemark;
 
-    /**
-     * 钢材质证书ID
-     */
+    @ApiModelProperty(value = "钢材质证书ID")
     private String steelQualityId;
 
-    /**
-     * 钢材质证书备注
-     */
+    @ApiModelProperty(value = "钢材质证书备注")
     private String steelQualityRemark;
 
-    /**
-     * 理化报告ID
-     */
+    @ApiModelProperty(value = "理化报告ID")
     private String physicochemicalIndexesId;
-    /**
-     * 理化报告备注
-     */
+
+    @ApiModelProperty(value = "理化报告备注")
     private String physicochemicalIndexesRemark;
-    /**
-     * 钢锭化学成分表ID
-     */
+
+    @ApiModelProperty(value = "钢锭化学成分表ID")
     private String ingotId;
-    /**
-     * 钢锭化学成分表备注
-     */
+
+    @ApiModelProperty(value = "钢锭化学成分表备注")
     private String ingotRemark;
 
 
