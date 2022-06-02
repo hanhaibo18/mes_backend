7b5cd10653507eb6b767505f57bbcbd6a4f31919
luzhiqiang <281747606@qq.com>
2022-06-02 10:45:31 +0800

    fix：修改计划保存更新时从后端取租户id

--- mes-modules/produce/src/main/java/com/richfit/mes/produce/controller/PlanController.java
+++ mes-modules/produce/src/main/java/com/richfit/mes/produce/controller/PlanController.java
@@ -6,8 +6,6 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.richfit.mes.common.core.api.CommonResult;
-import com.richfit.mes.common.core.api.IErrorCode;
-import com.richfit.mes.common.core.api.ResultCode;
 import com.richfit.mes.common.core.base.BaseController;
 import com.richfit.mes.common.core.base.BasePageDto;
 import com.richfit.mes.common.core.exception.GlobalException;
@@ -28,7 +26,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -50,12 +47,13 @@ public class PlanController extends BaseController {
 
     @Autowired
     private ActionService actionService;
+
     /**
      * 分页查询plan
      */
     @ApiOperation(value = "查询计划信息", notes = "根据查询条件返回计划信息")
     @ApiImplicitParams({
-            @ApiImplicitParam(name="queryDto",value="计划属性", paramType="BasePageDto")
+            @ApiImplicitParam(name = "queryDto", value = "计划属性", paramType = "BasePageDto")
     })
     @GetMapping("/query/page")
     public CommonResult queryByCondition(BasePageDto<String> queryDto) throws GlobalException {
@@ -67,22 +65,21 @@ public class PlanController extends BaseController {
             e.printStackTrace();
         }
 
-        if(null==planDto){
+        if (null == planDto) {
             planDto = new PlanDto();
         }
-        if(StringUtils.hasText(planDto.getOrderCol())){
+        if (StringUtils.hasText(planDto.getOrderCol())) {
             planDto.setOrderCol(StrUtil.toUnderlineCase(planDto.getOrderCol()));
-        }else{
+        } else {
             planDto.setOrderCol("status");
             planDto.setOrder("desc");
         }
 
 
-        IPage<Plan> planList = planService.queryPage(new Page<Plan>(queryDto.getPage(), queryDto.getLimit()),planDto);
+        IPage<Plan> planList = planService.queryPage(new Page<Plan>(queryDto.getPage(), queryDto.getLimit()), planDto);
 
         return CommonResult.success(planList);
     }
-
 
 
     /**
@@ -91,16 +88,14 @@ public class PlanController extends BaseController {
     @ApiOperation(value = "新增计划信息", notes = "新增计划信息")
     @ApiImplicitParam(name = "plan", value = "计划", required = true, dataType = "Plan", paramType = "body")
     @PostMapping("/save")
-    public CommonResult<Object> savePlan(@RequestBody Plan plan) throws GlobalException{
+    public CommonResult<Object> savePlan(@RequestBody Plan plan) throws GlobalException {
         TenantUserDetails user = SecurityUtils.getCurrentUser();
-        //plan.setTenantId(user.getTenantId());
-
+        plan.setTenantId(user.getTenantId());
         return planService.addPlan(plan);
     }
 
     /**
      * 根据ID获取计划
-     *
      */
     @ApiOperation(value = "获取计划信息", notes = "根据id获取计划详细信息")
     @ApiImplicitParam(name = "id", value = "计划ID", required = true, dataType = "String", paramType = "path")
@@ -117,8 +112,8 @@ public class PlanController extends BaseController {
     @ApiOperation(value = "修改计划信息", notes = "修改计划信息")
     @ApiImplicitParam(name = "plan", value = "计划", required = true, dataType = "Plan", paramType = "body")
     @PutMapping("/update")
-    public CommonResult<Object> updatePlan(@RequestBody Plan plan) throws GlobalException{
-
+    public CommonResult<Object> updatePlan(@RequestBody Plan plan) throws GlobalException {
+        plan.setTenantId(user.getTenantId());
         return planService.updatePlan(plan);
     }
 
@@ -128,26 +123,26 @@ public class PlanController extends BaseController {
     @ApiOperation(value = "删除计划信息", notes = "根据计划id删除计划记录")
     @ApiImplicitParam(name = "id", value = "计划id", required = true, dataType = "String", paramType = "path")
     @DeleteMapping("/delete/{id}")
-    public CommonResult<Boolean> delPlanById(@PathVariable String id) throws GlobalException{
+    public CommonResult<Boolean> delPlanById(@PathVariable String id) throws GlobalException {
         //计划状态为‘0’ 未开始时，才能删除
         Plan plan = planService.getById(id);
-        if(0!=plan.getStatus()){
+        if (0 != plan.getStatus()) {
             return CommonResult.failed("计划已开始，不能删除!");
         }
 
         Action action = new Action();
         action.setActionType("2");
         action.setActionItem("1");
-        action.setRemark("计划号：" + plan.getProjNum()+"，图号:"+plan.getDrawNo());
+        action.setRemark("计划号：" + plan.getProjNum() + "，图号:" + plan.getDrawNo());
         actionService.saveAction(action);
 
         return CommonResult.success(planService.delPlan(plan));
     }
 
 
-    public CommonResult<Boolean> updatePlanStatus(@PathVariable String id) throws GlobalException{
+    public CommonResult<Boolean> updatePlanStatus(@PathVariable String id) throws GlobalException {
 
-        boolean flag = planService.updatePlanStatus(id,"");
+        boolean flag = planService.updatePlanStatus(id, "");
 
         return CommonResult.success(flag);
 
@@ -159,7 +154,7 @@ public class PlanController extends BaseController {
     @ApiOperation(value = "计划进展评估数据", notes = "计划进展评估数据")
     @ApiImplicitParam(name = "id", value = "计划id", required = true, dataType = "String", paramType = "path")
     @GetMapping("/getPlanNeedHour/{id}")
-    public CommonResult<Map> getPlanNeedHour(@PathVariable String id) throws GlobalException{
+    public CommonResult<Map> getPlanNeedHour(@PathVariable String id) throws GlobalException {
 
         Plan plan = planService.getById(id);
 
@@ -169,13 +164,14 @@ public class PlanController extends BaseController {
 
     /**
      * 功能描述: 根据时间区间 和 图号批量获取计划
+     *
+     * @param planQueryDto 查询对象
      * @Author: xinYu.hou
      * @Date: 2022/4/20 15:27
-     * @param planQueryDto 查询对象
-     * @return: List<Map<String,String>>
+     * @return: List<Map < String, String>>
      **/
     @PostMapping("/queryPlan")
-    public List<Map<String, String>> getPlanList(@RequestBody PlanQueryDto planQueryDto){
-        return planService.getPlanList(planQueryDto.getStartTime(),planQueryDto.getEndTime(),planQueryDto.getDrawingNo(),planQueryDto.getTenantId(),planQueryDto.getBranchCode());
+    public List<Map<String, String>> getPlanList(@RequestBody PlanQueryDto planQueryDto) {
+        return planService.getPlanList(planQueryDto.getStartTime(), planQueryDto.getEndTime(), planQueryDto.getDrawingNo(), planQueryDto.getTenantId(), planQueryDto.getBranchCode());
     }
 }
