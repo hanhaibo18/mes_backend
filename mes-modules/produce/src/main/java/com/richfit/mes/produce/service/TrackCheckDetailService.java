e5215f2232eefbc04a059b35a266ee6ab66c3bad
luzhiqiang <281747606@.com>
2022-06-21 09:31:31 +0800

    feat:根据工序id查询质检的工序项目列表

--- mes-modules/produce/src/main/java/com/richfit/mes/produce/service/TrackCheckDetailService.java
+++ mes-modules/produce/src/main/java/com/richfit/mes/produce/service/TrackCheckDetailService.java
@@ -3,9 +3,19 @@ package com.richfit.mes.produce.service;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.richfit.mes.common.model.produce.TrackCheckDetail;
 
+import java.util.List;
+
 /**
  * @author 马峰
  * @Description 质检结果
  */
 public interface TrackCheckDetailService extends IService<TrackCheckDetail> {
+
+    /**
+     * 描述: 根据工序id查询质检的工序项目列表
+     *
+     * @Author: zhiqiang.lu
+     * @Date: 2022/6/21 10:25
+     **/
+    List<TrackCheckDetail> selectByTiId(String tiId);
 }
--- mes-modules/produce/src/main/java/com/richfit/mes/produce/service/TrackCheckDetailServiceImpl.java
+++ mes-modules/produce/src/main/java/com/richfit/mes/produce/service/TrackCheckDetailServiceImpl.java
@@ -1,19 +1,34 @@
 package com.richfit.mes.produce.service;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.richfit.mes.common.model.produce.TrackCheckDetail;
 import com.richfit.mes.produce.dao.TrackCheckDetailMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 /**
  * @author mafeng
  * @Description 质检明细
  */
 @Service
-public class TrackCheckDetailServiceImpl extends ServiceImpl<TrackCheckDetailMapper, TrackCheckDetail> implements TrackCheckDetailService{
+public class TrackCheckDetailServiceImpl extends ServiceImpl<TrackCheckDetailMapper, TrackCheckDetail> implements TrackCheckDetailService {
 
     @Autowired
     private TrackCheckDetailMapper trackCheckMapper;
 
+    /**
+     * 描述: 根据工序id查询质检的工序项目列表
+     *
+     * @Author: zhiqiang.lu
+     * @Date: 2022/6/21 10:25
+     **/
+    @Override
+    public List<TrackCheckDetail> selectByTiId(String tiId) {
+        QueryWrapper<TrackCheckDetail> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("ti_id", tiId);
+        return trackCheckMapper.selectList(queryWrapper);
+    }
 }
