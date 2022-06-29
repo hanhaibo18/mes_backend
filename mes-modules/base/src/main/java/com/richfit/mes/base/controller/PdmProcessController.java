9d51c9011ac04c8dcad2983529dde3bab4816efd
luzhiqiang <281747606@.com>
2022-06-29 17:13:50 +0800

    feat:添加同步到本地mes 接口

--- mes-modules/base/src/main/java/com/richfit/mes/base/controller/PdmProcessController.java
+++ mes-modules/base/src/main/java/com/richfit/mes/base/controller/PdmProcessController.java
@@ -1,10 +1,12 @@
 package com.richfit.mes.base.controller;
 
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.alibaba.fastjson.JSON;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.richfit.mes.base.service.*;
 import com.richfit.mes.common.core.api.CommonResult;
-import com.richfit.mes.common.model.base.*;
+import com.richfit.mes.common.model.base.PdmMesProcess;
+import com.richfit.mes.common.model.base.PdmProcess;
+import com.richfit.mes.common.security.util.SecurityUtils;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -45,6 +48,9 @@ public class PdmProcessController {
     @Autowired
     public OperatiponService OperationService;
 
+    @Autowired
+    private PdmMesProcessService pdmMesProcessService;
+
     @PostMapping(value = "/query/list")
     @ApiOperation(value = "工艺查询", notes = "工艺查询")
     @ApiImplicitParam(name = "pdmProcess", value = "工艺VO", required = true, dataType = "PdmProcess", paramType = "body")
@@ -64,110 +70,14 @@ public class PdmProcessController {
     @PostMapping("/synctomes")
     @ApiOperation(value = "同步到MES", notes = "同步到MES")
     @Transactional(rollbackFor = Exception.class)
-    public CommonResult<Router> synctomes(String processId, String version, String branchCode, String tenantId) {
-
-        PdmProcess pdmProcess = pdmProcessService.getOne(new QueryWrapper<PdmProcess>().eq("id", processId));
-        // 获取工序
-        List<PdmOption> pdmOptions = pdmOptionService.list(new QueryWrapper<PdmOption>().like("process_id", processId).orderByAsc("op_no"));
-        // 获取图纸
-        List<PdmDraw> pdmDraws = pdmDrawService.list(new QueryWrapper<PdmDraw>().eq("item_id", pdmProcess.getDrawNo()));
-        for (int i = 0; i < pdmOptions.size(); i++) {
-            //  List<PdmObject> pdmObjects = pdmObjectService.list(new QueryWrapper<PdmObject>().eq("op_id", pdmOptions.get(i)));
-        }
-        // 获取MES工艺
-        List<Router> routers = routerService.list(new QueryWrapper<Router>().eq("router_no", pdmProcess.getDrawNo()).eq("is_active", "1").like("branch_code", pdmProcess.getDataGroup()));
-        // 如果存在相同版本的工艺，则将工艺改为历史版本
-        boolean isAxistVer = false;
-        Router r = new Router();
-        for (int i = 0; i < routers.size(); i++) {
-            if (routers.get(i).getVersion().equals(pdmProcess.getRev())) {
-                isAxistVer = true;
-                r = routers.get(i);
-                r.setStatus("1");
-                r.setIsActive(("1"));
-                r.setTenantId(tenantId);
-                routerService.updateById(r);
-
-            } else {
-                r = routers.get(i);
-                r.setStatus("2");
-                r.setIsActive(("0"));
-                r.setTenantId(tenantId);
-                routerService.updateById(r);
-            }
-        }
-        if (routers.size() == 0) {
-            // 新增工艺
-            r = new Router();
-            r.setId(java.util.UUID.randomUUID().toString().replaceAll("-", ""));
-            r.setRouterNo(pdmProcess.getDrawNo());
-            r.setRouterName(pdmProcess.getName());
-            r.setVersion(pdmProcess.getRev());
-            r.setBranchCode(pdmProcess.getDataGroup());
-            if (pdmProcess.getProcessType().contains("机加")) {
-                r.setType("1");
-            } else if (pdmProcess.getProcessType().contains("装配")) {
-                r.setType("2");
-            } else {
-                r.setType("0");
-            }
-            r.setStatus("1");
-            r.setIsActive("1");
-            routerService.save(r);
-        }
-        // 获取插入的工艺
-        //List<Router> newrouters = routerService.list(new QueryWrapper<Router>().eq("router_no", pdmProcess.getDrawNo()).eq("status", "1").eq("is_active", "1").eq("version", pdmProcess.getRev()).like("branch_code", "%" + pdmProcess.getDataGroup() + "%"));
-        //r = new newrouters.get(0);
-        // 写入工序
-        List<Sequence> sequenceList = sequenceService.list(new QueryWrapper<Sequence>().eq("router_id", r.getId()));
-        for (int i = 0; i < sequenceList.size(); i++) {
-            sequenceService.removeById(sequenceList.get(i).getId());
-        }
-        for (int i = 0; i < pdmOptions.size(); i++) {
-            List<Operatipon> opts = OperationService.list(new QueryWrapper<Operatipon>().eq("opt_name", pdmOptions.get(i).getName()).eq("branch_code", pdmOptions.get(i).getDataGroup()));
-            Operatipon opt = new Operatipon();
-            if (opts.size() == 0) {
-
-                opt.setId(java.util.UUID.randomUUID().toString().replaceAll("-", ""));
-                opt.setOptCode(pdmOptions.get(i).getId());
-                opt.setBranchCode(pdmOptions.get(i).getDataGroup());
-                opt.setStatus(1);
-                opt.setOptName(pdmOptions.get(i).getName());
-                opt.setOptOrder(i + 1);
-                if (pdmOptions.get(i).getType().contains("机加")) {
-                    opt.setOptType(0);
-                } else if (pdmOptions.get(i).getType().contains("装配")) {
-                    opt.setOptType(2);
-                } else {
-                    opt.setOptType(0);
-                }
-                opt.setTenantId(tenantId);
-                OperationService.save(opt);
-
-            } else {
-                opt = opts.get(0);
-            }
-            Sequence sequence = new Sequence();
-            sequence.setBranchCode(opt.getBranchCode());
-            sequence.setOptCode(opt.getOptCode());
-            sequence.setIsParallel("0");
-            sequence.setOptOrder(Integer.parseInt(pdmOptions.get(i).getOpNo()));
-            sequence.setOptName(opt.getOptName());
-            sequence.setOptId(opt.getId());
-            sequence.setOptNextOrder(i + 2);
-            sequence.setOptType(String.valueOf(opt.getOptType()));
-            sequence.setRouterId(r.getId());
-            sequence.setStatus("1");
-            sequence.setTenantId(r.getTenantId());
-            sequence.setRemark(pdmOptions.get(i).getContent());
-            sequence.setTechnologySequence(pdmOptions.get(i).getOpNo());
-
-            sequenceService.save(sequence);
-        }
-
-
-        return CommonResult.success(r);
+    public void synctomes(String id) {
+        PdmProcess pdmProcess = pdmProcessService.getById(id);
+        pdmProcess.setItemStatus("已同步");
+        PdmMesProcess pdmMesProcess = JSON.parseObject(JSON.toJSONString(pdmProcess), PdmMesProcess.class);
+        pdmMesProcess.setItemStatus("待发布");
+        pdmMesProcess.setModifyTime(new Date());
+        pdmMesProcess.setModifyBy(SecurityUtils.getCurrentUser().getUsername());
+        pdmMesProcessService.saveOrUpdate(pdmMesProcess);
+        pdmProcessService.updateById(pdmProcess);
     }
-
-
 }
