7be182e7265a3be87533f07dc4f9b6ac748d7caa
luzhiqiang <281747606@.com>
2022-07-01 11:20:02 +0800

    feat:加入批量同步支持

--- mes-modules/base/src/main/java/com/richfit/mes/base/controller/PdmProcessController.java
+++ mes-modules/base/src/main/java/com/richfit/mes/base/controller/PdmProcessController.java
@@ -7,12 +7,10 @@ import com.richfit.mes.common.model.base.PdmProcess;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -64,7 +62,9 @@ public class PdmProcessController {
 
     @PostMapping("/synctomes")
     @ApiOperation(value = "同步到MES", notes = "同步到MES")
-    public void synctomes(String id) throws Exception {
-        pdmProcessService.synctomes(id);
+    public void synctomes(@ApiParam(value = "同步的工艺列表") @RequestBody List<PdmProcess> pdmProcesses) throws Exception {
+        for (PdmProcess p : pdmProcesses) {
+            pdmProcessService.synctomes(p.getDrawIdGroup());
+        }
     }
 }
