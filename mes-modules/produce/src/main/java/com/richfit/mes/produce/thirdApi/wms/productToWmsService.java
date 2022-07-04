9df5927fbb0c4ed53c7f2bc9fa456151315d48e9
luzhiqiang <281747606@.com>
2022-07-04 10:12:56 +0800

    doc:暂时注释掉错误代码

--- mes-modules/produce/src/main/java/com/richfit/mes/produce/thirdApi/wms/productToWmsService.java
+++ mes-modules/produce/src/main/java/com/richfit/mes/produce/thirdApi/wms/productToWmsService.java
@@ -1,19 +1,7 @@
 package com.richfit.mes.produce.thirdApi.wms;
 
-import cn.hutool.core.date.DateUtil;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.richfit.mes.common.model.produce.Certificate;
-import com.richfit.mes.common.security.util.SecurityUtils;
-import com.richfit.mes.produce.provider.SystemServiceClient;
-import com.richfit.mes.produce.utils.AESUtil;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
-import javax.annotation.PostConstruct;
-import java.util.Date;
 
 /**
  * 生产交库接口
@@ -25,48 +13,48 @@ import java.util.Date;
 @Service
 public class productToWmsService {
 
-    protected final RestTemplate restTemplate = new RestTemplate();
-
-    @Autowired
-    SystemServiceClient systemServiceClient;
-
-    private final String mesUploadAPiKey = "wms-url-jk";
-    private final String mesUrlKey = "wms-url-key";
-
-    private String mesUploadAPi = "";
-    private String mesToWmsApiKey = "";
-
-    @PostConstruct
-    private void init() {
-        mesUploadAPi = systemServiceClient.findItemParamByCode(mesUploadAPiKey).getData().getLabel();
-        mesToWmsApiKey = systemServiceClient.findItemParamByCode(mesUrlKey).getData().getLabel();
-    }
-
-    //接口格式，详见条码-mes接口文档
-    public Boolean sendRequest(Certificate cert) {
-
-        String sqd = cert.getCertificateNo() + DateUtil.format(new Date(), "MMddHHmmss");
-        String gc = SecurityUtils.getCurrentUser().getTenantErpCode();
-
-        String prod_No = "";
-        if (!cert.getProductNo().equals("") && cert.getProductNo().split(" ").length > 1) {
-            prod_No = cert.getProductNo().split(" ")[1];
-        }
-
-        String json = "{\"sqd\":\"" + sqd + "\",\"gc\":\"" + gc + "\",\"scdd\":\""
-                + cert.getProductNo() + "\",\"materialNum\":\"" + cert.getMaterialNo()
-                + "\",\"quantity\":" + cert.getNumber() + ",\"cp\":\"" + prod_No + "\",\"batchNum\":\"\",\"hgz\":\""
-                + cert.getCertificateNo() + "\",\"swFlag\":\"1\"}";
-
-        String url = mesUploadAPi + AESUtil.encrypt(json, mesToWmsApiKey);
-
-        ResponseEntity<JsonNode> resp = restTemplate.getForEntity(url, JsonNode.class);
-        log.debug("resp status is [{}],body is [{}]", resp.getStatusCode(), resp.getBody());
-        String retStatus = resp.getBody().get("retStatus").asText();
-        log.debug("retMsg is [{}]", resp.getBody().get("retMsg").asText());
-
-        return "Y".equals(retStatus);
-    }
+//    protected final RestTemplate restTemplate = new RestTemplate();
+//
+//    @Autowired
+//    SystemServiceClient systemServiceClient;
+//
+//    private final String mesUploadAPiKey = "wms-url-jk";
+//    private final String mesUrlKey = "wms-url-key";
+//
+//    private String mesUploadAPi = "";
+//    private String mesToWmsApiKey = "";
+//
+//    @PostConstruct
+//    private void init() {
+//        mesUploadAPi = systemServiceClient.findItemParamByCode(mesUploadAPiKey).getData().getLabel();
+//        mesToWmsApiKey = systemServiceClient.findItemParamByCode(mesUrlKey).getData().getLabel();
+//    }
+//
+//    //接口格式，详见条码-mes接口文档
+//    public Boolean sendRequest(Certificate cert) {
+//
+//        String sqd = cert.getCertificateNo() + DateUtil.format(new Date(), "MMddHHmmss");
+//        String gc = SecurityUtils.getCurrentUser().getTenantErpCode();
+//
+//        String prod_No = "";
+//        if (!cert.getProductNo().equals("") && cert.getProductNo().split(" ").length > 1) {
+//            prod_No = cert.getProductNo().split(" ")[1];
+//        }
+//
+//        String json = "{\"sqd\":\"" + sqd + "\",\"gc\":\"" + gc + "\",\"scdd\":\""
+//                + cert.getProductNo() + "\",\"materialNum\":\"" + cert.getMaterialNo()
+//                + "\",\"quantity\":" + cert.getNumber() + ",\"cp\":\"" + prod_No + "\",\"batchNum\":\"\",\"hgz\":\""
+//                + cert.getCertificateNo() + "\",\"swFlag\":\"1\"}";
+//
+//        String url = mesUploadAPi + AESUtil.encrypt(json, mesToWmsApiKey);
+//
+//        ResponseEntity<JsonNode> resp = restTemplate.getForEntity(url, JsonNode.class);
+//        log.debug("resp status is [{}],body is [{}]", resp.getStatusCode(), resp.getBody());
+//        String retStatus = resp.getBody().get("retStatus").asText();
+//        log.debug("retMsg is [{}]", resp.getBody().get("retMsg").asText());
+//
+//        return "Y".equals(retStatus);
+//    }
 
 
 }
