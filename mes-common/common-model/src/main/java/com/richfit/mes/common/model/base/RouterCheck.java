f6b12ed3986ee16481a105766d7bf6d734059506
mafeng02 <mafeng02@cnpc.com.cn>
2022-07-06 12:38:45 +0800

    fixed:工序注意事项

--- mes-common/common-model/src/main/java/com/richfit/mes/common/model/base/RouterCheck.java
+++ mes-common/common-model/src/main/java/com/richfit/mes/common/model/base/RouterCheck.java
@@ -17,7 +17,7 @@ public class RouterCheck extends BaseEntity<RouterCheck> {
      * 租户ID
      */
     private String tenantId;
-    
+
       /**
      * 机构编码
      */
@@ -47,32 +47,32 @@ public class RouterCheck extends BaseEntity<RouterCheck> {
      * 规则类型 0-人工判断 1-枚举 2-区间 3-允差
      */
     private String type;
-    
+
         /**
      * 状态
      */
     private String status;
-    
+
         /**
      * 顺序
      */
     private int checkOrder;
-    
+
         /**
      * 单位
      */
     private String unit;
-    
+
         /**
-     * 方法
+     * 方法 枚举-技术要求，检查内容，注意事项，质量资料
      */
     private String method;
-    
+
         /**
      * 错误提示
      */
     private String errorMsg;
-    
+
         /**
      * 是否为空
      */
@@ -87,7 +87,7 @@ public class RouterCheck extends BaseEntity<RouterCheck> {
      * 创建人
      */
     private String createBy;
-    
+
         /**
      * 创建时间
      */
@@ -97,13 +97,25 @@ public class RouterCheck extends BaseEntity<RouterCheck> {
      * 修改人
      */
     private String modifyBy;
-    
+
         /**
      * 修改时间
      */
     private Date modifyTime;
-     
 
 
+    private String propertySymbol;
+    private String propertyNum;
+    private String propertyRule;
+    private String propertyUplimit;
+    private String propertyLowerlimit;
+    private String propertyTestmethod;
+    private String propertyInputtype;
+    private String propertyDefaultvalue;
+    private String propertyDatatype;
+    private String propertyIndicatorname;
+    private String propertyObjectname;
+    private String propertyField;
+    private String propertyUnit;
 
 }
