a31e0314b71cdc739f4599de3e21219f3ec29c93
gaoliang01 <gaoliang01@cnpc.com.cn>
2022-07-26 08:56:04 +0800

    doc:接口注解完善

--- mes-integration/erp-sap/src/main/java/com/kld/mes/erp/controller/CertWorkHourController.java
+++ mes-integration/erp-sap/src/main/java/com/kld/mes/erp/controller/CertWorkHourController.java
@@ -8,10 +8,7 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,7 +17,7 @@ import java.util.List;
  * @Date: 2022/7/21 14:29
  */
 @Slf4j
-@Api("ERP工时接口")
+@Api(value = "ERP接口封装", tags = {"跟单工时推送接口"})
 @RestController
 @RequestMapping("/api/integration/erp/work-hour")
 public class CertWorkHourController {
@@ -31,14 +28,18 @@ public class CertWorkHourController {
     @ApiOperation(value = "工时推送", notes = "根据跟单工序推送工时数据到ERP")
     @PostMapping("/push")
     public CommonResult<Boolean> certWorkHourPush(@ApiParam(value = "工序列表") @RequestBody List<TrackItem> trackItemList,
-                                                  @ApiParam(value = "erp代号") String erpCode,
-                                                  @ApiParam(value = "订单号") String orderNo,
-                                                  @ApiParam(value = "数量") int qty,
-                                                  @ApiParam(value = "单位") String unit) {
+                                                  @ApiParam(value = "erp代号") @RequestParam String erpCode,
+                                                  @ApiParam(value = "订单号") @RequestParam String orderNo,
+                                                  @ApiParam(value = "数量") @RequestParam int qty,
+                                                  @ApiParam(value = "单位") @RequestParam String unit) {
 
         boolean b = certWorkHourService.sendWorkHour(trackItemList, erpCode, orderNo, qty, unit);
 
-        return new CommonResult(b);
+        if (b) {
+            return CommonResult.success(b);
+        } else {
+            return CommonResult.failed("报工失败");
+        }
 
     }
 
