98f5886326f842df60760ff93abc522ba4c8bcbc
renzewen <12345678>
2022-08-02 17:40:09 +0800

    feat:上传申请单保存申请单及物料信息接口

---
+++ mes-modules/produce/src/main/java/com/richfit/mes/produce/service/RequestNoteDetailService.java
@@ -0,0 +1,8 @@
+package com.richfit.mes.produce.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.richfit.mes.common.model.produce.RequestNoteDetail;
+
+public interface RequestNoteDetailService extends IService<RequestNoteDetail> {
+
+}
---
+++ mes-modules/produce/src/main/java/com/richfit/mes/produce/service/RequestNoteDetailServiceImpl.java
@@ -0,0 +1,13 @@
+package com.richfit.mes.produce.service;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.richfit.mes.common.model.produce.RequestNoteDetail;
+import com.richfit.mes.produce.dao.RequestNoteDetailMapper;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class RequestNoteDetailServiceImpl extends ServiceImpl<RequestNoteDetailMapper, RequestNoteDetail> implements RequestNoteDetailService {
+
+
+}
