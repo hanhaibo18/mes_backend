68829f7e02b7d85460c0aaba5db9dac4bb12fd37
houxinyu <houxinyu@cnpc.com.cn>
2022-08-03 17:14:12 +0800

    # feat: 增加下工序实体

---
+++ mes-modules/produce/src/main/java/com/richfit/mes/produce/service/NextProcessService.java
@@ -0,0 +1,14 @@
+package com.richfit.mes.produce.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.richfit.mes.common.model.produce.NextProcess;
+
+/**
+ * @ClassName: NextProcessService.java
+ * @Author: Hou XinYu
+ * @Description: TODO
+ * @CreateTime: 2022年08月03日 10:36:00
+ */
+public interface NextProcessService extends IService<NextProcess> {
+    
+}
