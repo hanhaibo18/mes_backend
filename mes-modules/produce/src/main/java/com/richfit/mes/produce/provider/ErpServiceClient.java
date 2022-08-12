1140cafdf39c60a8e572f0c8e1c96512903828f0
luzhiqiang <281747606@.com>
2022-08-12 15:53:50 +0800

    feat:修改wms 改用接口服务，并天津齐套erp库存数量，晚上齐套数量计算功能

--- mes-modules/produce/src/main/java/com/richfit/mes/produce/provider/ErpServiceClient.java
+++ mes-modules/produce/src/main/java/com/richfit/mes/produce/provider/ErpServiceClient.java
@@ -1,9 +1,12 @@
 package com.richfit.mes.produce.provider;
 
 import com.richfit.mes.common.core.api.CommonResult;
+import com.richfit.mes.common.model.base.Product;
 import com.richfit.mes.common.model.produce.TrackItem;
 import com.richfit.mes.produce.provider.fallback.ErpServiceClientFallbackImpl;
+import io.swagger.annotations.ApiParam;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -24,5 +27,17 @@ public interface ErpServiceClient {
                                                   @RequestParam(value = "订单号") String orderNo,
                                                   @RequestParam(value = "数量") int qty,
                                                   @RequestParam(value = "单位") String unit);
+
+    /**
+     * 功能描述: ERP库存查询
+     *
+     * @param materialNos 物料号码
+     * @param erpCode     erpCode
+     * @Author: zhiqiang.lu
+     * @Date: 2022/8/12 11:37
+     **/
+    @GetMapping("/api/integration/erp/storage/getStorage")
+    public CommonResult<List<Product>> getStorage(@ApiParam(value = "物料号") @RequestParam String materialNos,
+                                                  @ApiParam(value = "erp代号") @RequestParam String erpCode);
 
 }
