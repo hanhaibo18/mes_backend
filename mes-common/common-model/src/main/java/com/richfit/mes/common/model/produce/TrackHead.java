56306b0d8e29914e8129431488d61a5d2e131553
luzhiqiang <281747606@.com>
2022-08-18 12:44:28 +0800

    fix:修改跟单实体类，注释无用属性

--- mes-common/common-model/src/main/java/com/richfit/mes/common/model/produce/TrackHead.java
+++ mes-common/common-model/src/main/java/com/richfit/mes/common/model/produce/TrackHead.java
@@ -3,6 +3,7 @@ package com.richfit.mes.common.model.produce;
 import com.baomidou.mybatisplus.annotation.FieldStrategy;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.richfit.mes.common.core.base.BaseEntity;
+import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
@@ -15,331 +16,208 @@ import java.util.Map;
  * @Description 跟单
  */
 @Data
+@ApiModel(value = "跟单管理")
 public class TrackHead extends BaseEntity<TrackHead> {
 
     private static final long serialVersionUID = 6336423092552908350L;
-    /**
-     * 租户ID
-     */
+
+    @ApiModelProperty(value = "租户ID")
     private String tenantId;
 
-    /**
-     * 跟单号
-     */
+    @ApiModelProperty(value = "跟单号")
     private String trackNo;
 
-    /**
-     * 跟单类型 0单件 1批次
-     */
+    @ApiModelProperty(value = "跟单类型 0单件 1批次")
     private String trackType;
 
-    /**
-     * 工作号
-     */
+    @ApiModelProperty(value = "工作号")
     private String workNo;
 
-    /**
-     * 产品编号
-     */
+    @ApiModelProperty(value = "产品编号")
     private String productNo;
 
-    /**
-     * 产品编号描述
-     */
+    @ApiModelProperty(value = "产品编号描述")
     private String productNoDesc;
 
-    /**
-     * 物料编号
-     */
+    @ApiModelProperty(value = "物料编号")
     private String materialNo;
 
-    /**
-     * 图号
-     */
+    @ApiModelProperty(value = "图号")
     private String drawingNo;
 
-    /**
-     * 生产订单编号
-     */
+    @ApiModelProperty(value = "生产订单编号")
     private String productionOrder;
 
-    /**
-     * 审批人
-     */
+    @ApiModelProperty(value = "审批人")
     private String approvalBy;
 
-    /**
-     * 审批时间
-     */
+    @ApiModelProperty(value = "审批时间")
     private Date approvalTime;
 
-    /**
-     * 审批状态 0待审批 1通过 2打回
-     */
+    @ApiModelProperty(value = "审批状态 0待审批 1通过 2打回")
     private String approvalStatus;
 
-    /**
-     * 状态 0已生成待派工 1在制 2完工 3作废 4删除
-     */
+    @ApiModelProperty(value = "状态 0已生成待派工 1在制 2完工 3作废 4删除")
     private String status;
 
-    /**
-     * 试棒数量
-     */
+    @ApiModelProperty(value = "试棒数量")
     private Integer testBarNumber;
 
-    /**
-     * 试棒类型
-     */
+    @ApiModelProperty(value = "试棒类型")
     private String testBarType;
 
-    /**
-     * 炉批号
-     */
+    @ApiModelProperty(value = "炉批号")
     private String batchNo;
 
-    /**
-     * 准备工时 0首件 1全部
-     */
+    @ApiModelProperty(value = "准备工时 0首件 1全部")
     private String isFirst;
 
-    /**
-     * 优先级
-     */
+    @ApiModelProperty(value = "优先级")
     private String priority;
 
-    /**
-     * 数量
-     */
+    @ApiModelProperty(value = "数量")
     private Integer number;
 
-    /**
-     * 签发人
-     */
+    @ApiModelProperty(value = "签发人")
     private String issueBy;
 
-    /**
-     * 签发时间
-     */
+    @ApiModelProperty(value = "签发时间")
     private Date issueTime;
 
-    /**
-     * 来料合格证号
-     */
+    @ApiModelProperty(value = "来料合格证号")
     private String materialCertificateNo;
 
-    /**
-     * 试棒跟单编号
-     */
+    @ApiModelProperty(value = "试棒跟单编号")
     private String testBarTrackNo;
 
-    /**
-     * 合格证号
-     */
+    @ApiModelProperty(value = "合格证号")
     @TableField(updateStrategy = FieldStrategy.IGNORED)
     private String certificateNo;
 
-    /**
-     * 合同编号
-     */
+    @ApiModelProperty(value = "合同编号")
     private String contractNo;
 
-    /**
-     * 计划号
-     */
+    @ApiModelProperty(value = "计划号")
     private String workPlanNo;
 
-    /**
-     * 组织机构编号
-     */
+    @ApiModelProperty(value = "组织机构编号")
     private String branchCode;
 
-    /**
-     * 工序版本号
-     */
+    @ApiModelProperty(value = "工序版本号")
     private String routerVer;
 
-    /**
-     * 跟单完工时间
-     */
+    @ApiModelProperty(value = "跟单完工时间")
     private Date completeTime;
 
-    /**
-     * 跟单工序
-     */
+    @ApiModelProperty(value = "跟单工序")
     @TableField(exist = false)
     private List<TrackItem> trackItems;
 
-    /**
-     * 跟单库存使用列表
-     */
+    @ApiModelProperty(value = "物料产品信息列表，用于根据物料信息生产跟单")
     @TableField(exist = false)
     private List<Map> storeList;
 
 
-    private String userMaterialNo;
-
-    @TableField(exist = false)
-    private String optName;
-
-    @TableField(exist = false)
-    private String optId;
-
-    @TableField(exist = false)
-    private String tiId;
-
-    @TableField(exist = false)
-    private String sequenceOrderBy;
-
-    private String userProductNo;
-
-    private Integer startNo;
-    private Integer endNo;
-    private String suffixNo;
-
     @ApiModelProperty(value = "零部件名称", dataType = "String")
     private String materialName;
+
     @ApiModelProperty(value = "产品名称", dataType = "String")
     private String productName;
+
     @ApiModelProperty(value = "代用材料", dataType = "String")
     private String replaceMaterial;
+
     @ApiModelProperty(value = "重量", dataType = "Float")
     private Float weight;
+
     @ApiModelProperty(value = "材质", dataType = "String")
     private String texture;
 
+    @ApiModelProperty(value = "材质", dataType = "String")
     private String templateCode;
 
-    @TableField(exist = false)
-    private String selectedMaterialNo;
-    /**
-     * 描述: 计划Id
-     *
-     * @Author: xinYu.hou
-     * @Date: 2022/4/19 10:25
-     **/
+
+    @ApiModelProperty(value = "计划Id", dataType = "String")
     private String workPlanId;
-    /**
-     * 描述: 工艺id
-     *
-     * @Author: zhiqiang.lu
-     * @Date: 2022/6/13 10:25
-     **/
+
+    @ApiModelProperty(value = "工艺id", dataType = "String")
     private String routerId;
-    /**
-     * 描述: 跟单类型 0库存生成  1物料生成  2计划生成
-     *
-     * @Author: zhiqiang.lu
-     * @Date: 2022/6/13 10:25
-     **/
+
+    @ApiModelProperty(value = "跟单类型 0库存生成  1物料生成  2计划生成", dataType = "String")
     private String type;
 
-    /**
-     * 描述: 是否试棒跟单  0否  1是
-     *
-     * @Author: zhiqiang.lu
-     * @Date: 2022/6/16 10:25
-     **/
+    @ApiModelProperty(value = "是否试棒跟单  0否  1是", dataType = "String")
     private String isTestBar;
 
-    /**
-     * 描述: 项目bomID
-     *
-     * @Author: zhiqiang.lu
-     * @Date: 2022/6/27 10:25
-     **/
+    @ApiModelProperty(value = "项目bomID", dataType = "String")
     private String projectBomId;
 
-    /**
-     * 关联项目BOM的workno
-     */
+    @ApiModelProperty(value = "关联项目BOM的workno", dataType = "String")
     private String projectBomWork;
 
-    /**
-     * 关联项目BOM的名称
-     */
+    @ApiModelProperty(value = "联项目BOM的名称", dataType = "String")
     private String projectBomName;
 
-    /**
-     * bom分组选择
-     */
+    @ApiModelProperty(value = "bom分组选择", dataType = "String")
     private String projectBomGroup;
 
-    /**
-     * 描述: 跟单分类：1机加  2装配 3热处理 4钢结构
-     *
-     * @Author: zhiqiang.lu
-     * @Date: 2022/6/28 10:25
-     **/
+    @ApiModelProperty(value = "跟单分类：1机加  2装配 3热处理 4钢结构", dataType = "String")
     private String classes;
 
-
-    /**
-     * 描述: 完工资料：Y是
-     *
-     * @Author: zhiqiang.lu
-     * @Date: 2022/7/21 10:25
-     **/
+    @ApiModelProperty(value = "完工资料：Y是", dataType = "String")
     private String isCompletionData;
 
-    /**
-     * 描述: 是否批次：Y是 N否
-     *
-     * @Author: zhiqiang.lu
-     * @Date: 2022/7/26 10:25
-     **/
+    @ApiModelProperty(value = "是否批次：Y是 N否", dataType = "String")
     private String isBatch;
 
-    /**
-     * 描述: 完成数量
-     *
-     * @Author: zhiqiang.lu
-     * @Date: 2022/7/26 10:25
-     **/
+    @ApiModelProperty(value = "完成数量", dataType = "Integer")
     private Integer numberComplete;
 
-
     @TableField(exist = false)
-    private String finalProductNo;
-    /**
-     * 描述: 分流id
-     *
-     * @Author: zhiqiang.lu
-     * @Date: 2022/7/26 10:25
-     **/
-    @TableField(exist = false)
+    @ApiModelProperty(value = "分流id", dataType = "String")
     private String flowId;
 
-
-    /**
-     * 描述: 原跟单id
-     *
-     * @Author: zhiqiang.lu
-     * @Date: 2022/8/4 10:25
-     **/
+    @ApiModelProperty(value = "原跟单id", dataType = "String")
     private String originalTrackId;
 
-    /**
-     * 描述: 原跟单编号
-     *
-     * @Author: zhiqiang.lu
-     * @Date: 2022/8/4 10:25
-     **/
+    @ApiModelProperty(value = "原跟单编号", dataType = "String")
     private String originalTrackNo;
 
-    /**
-     * 描述: 分流（生产线数量）
-     *
-     * @Author: zhiqiang.lu
-     * @Date: 2022/8/5 10:25
-     **/
+    @ApiModelProperty(value = "分流（生产线数量）", dataType = "Integer")
     private Integer flowNumber;
 
-    /**
-     * 描述: 订单id
-     *
-     * @Author: zhiqiang.lu
-     * @Date: 2022/8/5 10:25
-     **/
+    @ApiModelProperty(value = "订单id", dataType = "String")
     private String productionOrderId;
+
+    @TableField(exist = false)
+    @ApiModelProperty(value = "选择的毛胚物料号码", dataType = "String")
+    private String selectedMaterialNo;
+
+    //无用属性
+    private String userProductNo;
+    private Integer startNo;
+    private Integer endNo;
+    private String suffixNo;
+    private String userMaterialNo;
+
+
+//    @TableField(exist = false)
+//    @ApiModelProperty(value = "项目号")
+//    private String sequenceOrderBy;
+
+//    @TableField(exist = false)
+//    @ApiModelProperty(value = "项目号")
+//    private String optName;
+
+//    @TableField(exist = false)
+//    @ApiModelProperty(value = "项目号")
+//    private String optId;
+
+//    @TableField(exist = false)
+//    @ApiModelProperty(value = "项目号")
+//    private String tiId;
+
+//    @TableField(exist = false)
+//    @ApiModelProperty(value = "材质", dataType = "String")
+//    private String finalProductNo;
 }
