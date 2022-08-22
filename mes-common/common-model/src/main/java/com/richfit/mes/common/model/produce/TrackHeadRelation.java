abb9c09695999c608d0405fa12f582054146016c
luzhiqiang <281747606@.com>
2022-08-22 09:10:25 +0800

    fix:代码细节处理

--- mes-common/common-model/src/main/java/com/richfit/mes/common/model/produce/TrackHeadRelation.java
+++ mes-common/common-model/src/main/java/com/richfit/mes/common/model/produce/TrackHeadRelation.java
@@ -1,5 +1,6 @@
 package com.richfit.mes.common.model.produce;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 /**
@@ -15,31 +16,37 @@ public class TrackHeadRelation {
      * @Author: zhiqiang.lu
      * @Date: 2022/6/20 10:25
      **/
+    @ApiModelProperty(value = "主键")
     private long id;
 
     /**
      * 跟单ID
      */
+    @ApiModelProperty(value = "跟单ID")
     private String thId;
 
     /**
      * 跟单分流ID
      */
+    @ApiModelProperty(value = "跟单分流ID")
     private String flowId;
 
     /**
      * 库存ID
      */
+    @ApiModelProperty(value = "库存ID")
     private String lsId;
 
     /**
      * 类型 0输入物料 1输出物料
      */
+    @ApiModelProperty(value = "类型 0输入物料 1输出物料")
     private String type;
 
     /**
      * 数量
      */
+    @ApiModelProperty(value = "数量")
     private Integer number;
 
 }
