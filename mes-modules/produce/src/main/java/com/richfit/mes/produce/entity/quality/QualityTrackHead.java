6a87157ca8baf79605f557b9f1963038c8ff5ea6
luzhiqiang <281747606@.com>
2022-08-24 11:41:52 +0800

    feat:质量管理租户跟单实体扩展

---
+++ mes-modules/produce/src/main/java/com/richfit/mes/produce/entity/quality/QualityTrackHead.java
@@ -0,0 +1,7 @@
+package com.richfit.mes.produce.entity.quality;
+
+import com.richfit.mes.common.model.produce.TrackHead;
+
+public class QualityTrackHead extends TrackHead {
+    private static final long serialVersionUID = -1292037071987758162L;
+}
