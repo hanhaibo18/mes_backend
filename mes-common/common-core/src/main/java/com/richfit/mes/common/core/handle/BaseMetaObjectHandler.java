3db68e1c42a110cda8548f79edd8cd039db95b5c
suncun <suncun@cnpc.com.cn>
2022-08-24 09:42:17 +0800

    feat:mybatis-plus提供的strictUpdateFill默认方法的策略均为:如果属性有值则不覆盖,如果填充值为null则不填充,修改为setFieldValByName

--- mes-common/common-core/src/main/java/com/richfit/mes/common/core/handle/BaseMetaObjectHandler.java
+++ mes-common/common-core/src/main/java/com/richfit/mes/common/core/handle/BaseMetaObjectHandler.java
@@ -34,7 +34,9 @@ public class BaseMetaObjectHandler implements MetaObjectHandler {
 
     @Override
     public void updateFill(MetaObject metaObject) {
-        this.strictInsertFill(metaObject, "modifyBy", String.class, getCurrentUsername());
-        this.strictInsertFill(metaObject, "modifyTime", Date.class, new Date());
+        //mybatis-plus提供的strictUpdateFill默认方法的策略均为:如果属性有值则不覆盖,如果填充值为null则不填充,修改为setFieldValByName
+        this.setFieldValByName("modifyTime",new Date(),metaObject);
+        this.setFieldValByName("modifyBy",getCurrentUsername(),metaObject);
+
     }
 }
