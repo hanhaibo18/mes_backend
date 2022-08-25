581e3ab6f6a8757cab2599a74fd2d7fed0e7b9b2
houxinyu <houxinyu@cnpc.com.cn>
2022-08-25 15:02:58 +0800

    # fix: UserRole中间表增加UserType字段

--- mes-common/common-model/src/main/java/com/richfit/mes/common/model/sys/UserRole.java
+++ mes-common/common-model/src/main/java/com/richfit/mes/common/model/sys/UserRole.java
@@ -1,6 +1,7 @@
 package com.richfit.mes.common.model.sys;
 
 import com.richfit.mes.common.core.base.BaseEntity;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
@@ -9,6 +10,8 @@ import lombok.experimental.Accessors;
 /**
  * @author sun
  * @Description 角色
+ * @modifierTime: 2022年8月25日14:53:51
+ * @modifier: hou
  */
 @Data
 @EqualsAndHashCode(callSuper = true)
@@ -23,4 +26,8 @@ public class UserRole extends BaseEntity<UserRole> {
      * 角色ID
      */
     private String roleId;
+
+    @ApiModelProperty(value = "用户类型(0=普通,1=普通质检,2=质检租户)", dataType = "String")
+    private String userType;
+
 }
