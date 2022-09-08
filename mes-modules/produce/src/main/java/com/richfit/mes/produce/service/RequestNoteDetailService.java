c1ddb59dd914528b998bbbfad810af9dd22e6e7a
luzhiqiang <281747606@.com>
2022-09-08 14:40:45 +0800

    feat:装配配送查询后端计算配送数据计算接口

--- mes-modules/produce/src/main/java/com/richfit/mes/produce/service/RequestNoteDetailService.java
+++ mes-modules/produce/src/main/java/com/richfit/mes/produce/service/RequestNoteDetailService.java
@@ -3,6 +3,17 @@ package com.richfit.mes.produce.service;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.richfit.mes.common.model.produce.RequestNoteDetail;
 
+import java.util.List;
+
 public interface RequestNoteDetailService extends IService<RequestNoteDetail> {
 
+
+    /**
+     * 功能描述: 通过配送主表id查询配送零件详细信息
+     *
+     * @param noteId 配送主表id
+     * @Author: zhiqiang.lu
+     * @Date: 2022/9/8 14:32
+     **/
+    List<RequestNoteDetail> getDeliveryInformation(String noteId);
 }
