914f84f3cfe56ed3ee5aa7ceac5e574d436a3e50
gaoliang01 <gaoliang01@cnpc.com.cn>
2022-09-26 17:11:51 +0800

    fix：修改system.out用法，增加未完成功能TODO标记

--- mes-modules/produce/src/main/java/com/richfit/mes/produce/task/PushWorkHourTask.java
+++ mes-modules/produce/src/main/java/com/richfit/mes/produce/task/PushWorkHourTask.java
@@ -2,15 +2,13 @@ package com.richfit.mes.produce.task;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.richfit.mes.common.model.produce.Certificate;
-import com.richfit.mes.common.model.produce.TrackCertificate;
 import com.richfit.mes.produce.service.CertificateService;
-import com.richfit.mes.produce.service.TrackCertificateService;
 import com.richfit.mes.produce.service.bsns.CertAdditionalBsns;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -24,6 +22,7 @@ import java.util.List;
 
 @Configuration
 @EnableScheduling   //开启定时任务
+@Slf4j
 public class PushWorkHourTask {
 
     @Autowired
@@ -38,13 +37,20 @@ public class PushWorkHourTask {
     //添加定时任务
     //@Scheduled(cron = "${time.push_work_hour}")
     private void execTask() {
-        System.err.println("执行定时任务时间: " + LocalDateTime.now());
+        log.debug("执行定时任务时间: " + LocalDateTime.now());
+
+        //TODO 如果是装配车间，切开启了定时任务
+        //查询完工 或 已生成完工资料，且未生成合格证的跟单  =》 先生成合格证，再推送
+
+
         // 查出没有推送给ERP的合格证
         QueryWrapper<Certificate> queryWrapper = new QueryWrapper<>();
         queryWrapper.ne("is_send_work_hour", 1);
         queryWrapper.ge("create_time", taskDate);
         List<Certificate> list = certificateService.list(queryWrapper);
-        list.forEach(c -> {certAdditionalBsns.pushWorkHour(c);});
+        list.forEach(c -> {
+            certAdditionalBsns.pushWorkHour(c);
+        });
     }
 
 }
