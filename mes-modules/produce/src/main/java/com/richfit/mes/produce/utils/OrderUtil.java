f99f30bd0568c191ea2b536753f358eddeb7de3d
luzhiqiang <281747606@.com>
2022-09-28 14:39:48 +0800

    fix:排序工具类整理，优化工具，并添加sql版字符串。

--- mes-modules/produce/src/main/java/com/richfit/mes/produce/utils/OrderUtil.java
+++ mes-modules/produce/src/main/java/com/richfit/mes/produce/utils/OrderUtil.java
@@ -31,18 +31,82 @@ public class OrderUtil {
      * @Date: 2022.9.27
      */
     public static void query(QueryWrapper queryWrapper, String orderCol, String order) {
+        query(queryWrapper, null, orderCol, order);
+    }
+
+    /**
+     * 功能描述: 排序通用方法(用于带有代理表名的)
+     *
+     * @param queryWrapper 查询
+     * @param table        代理表名
+     * @param orderCol     排序列
+     * @param order        排序方式
+     * @Author: zhiqiang.lu
+     * @Date: 2022.9.28
+     */
+    public static void query(QueryWrapper queryWrapper, String table, String orderCol, String order) {
+        table = tableName(table);
         if (!StrUtil.isBlank(orderCol)) {
             if (!StrUtil.isBlank(order)) {
                 if (ORDER_ASC.equals(order)) {
-                    queryWrapper.orderByDesc(StrUtil.toUnderlineCase(orderCol));
+                    queryWrapper.orderByDesc(table + StrUtil.toUnderlineCase(orderCol));
                 } else if (ORDER_DESC.equals(order)) {
-                    queryWrapper.orderByAsc(StrUtil.toUnderlineCase(orderCol));
+                    queryWrapper.orderByAsc(table + StrUtil.toUnderlineCase(orderCol));
                 }
             } else {
-                queryWrapper.orderByDesc(StrUtil.toUnderlineCase(orderCol));
+                queryWrapper.orderByDesc(table + StrUtil.toUnderlineCase(orderCol));
             }
         } else {
-            queryWrapper.orderByDesc("modify_time");
+            queryWrapper.orderByDesc(table + "modify_time");
+        }
+    }
+
+    /**
+     * 功能描述: 排序sql字符串方法
+     *
+     * @param orderCol 排序列
+     * @param order    排序方式
+     * @Author: zhiqiang.lu
+     * @Date: 2022.9.28
+     */
+    public static String querySql(String orderCol, String order) {
+        return querySql(null, orderCol, order);
+    }
+
+    /**
+     * 功能描述: 排序sql字符串方法(用于带有代理表名的)
+     *
+     * @param table    代理表名
+     * @param orderCol 排序列
+     * @param order    排序方式
+     * @Author: zhiqiang.lu
+     * @Date: 2022.9.28
+     */
+    public static String querySql(String table, String orderCol, String order) {
+        table = tableName(table);
+        if (!StrUtil.isBlank(orderCol)) {
+            if (!StrUtil.isBlank(order)) {
+                return "order by " + table + orderCol + " " + order;
+            } else {
+                return "order by " + table + orderCol + " desc";
+            }
+        } else {
+            return "order by " + table + "modify_time desc";
+        }
+    }
+
+    /**
+     * 功能描述: 代理表面处理(例如：t.modify_time查询)
+     *
+     * @param table 代理表名
+     * @Author: zhiqiang.lu
+     * @Date: 2022.9.28
+     */
+    public static String tableName(String table) {
+        if (StrUtil.isBlank(table)) {
+            return "";
+        } else {
+            return table + ".";
         }
     }
 }
