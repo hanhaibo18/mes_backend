6804a01d3cc8cb25243950c09085d6808441cbb0
panshi.zhang <2532263296@qq.com>
2022-10-27 09:58:46 +0800

    fix:合格证编辑回显时后端添加产品编号和数量字段

--- mes-common/common-model/src/main/java/com/richfit/mes/common/model/produce/TrackItem.java
+++ mes-common/common-model/src/main/java/com/richfit/mes/common/model/produce/TrackItem.java
@@ -398,6 +398,7 @@ public class TrackItem extends BaseEntity<TrackItem> {
     private String auditBy;
 
     @ApiModelProperty(value = "是否发起委托")
+    @TableField(exist = false)
     private String isEntrust;
 
     @ApiModelProperty(value = "材料委托单")
