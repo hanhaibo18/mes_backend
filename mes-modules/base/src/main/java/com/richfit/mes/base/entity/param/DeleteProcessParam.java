8eefe033a00fabf6922b5742f2d4880b0ffaa3f1
hujia <18911024287@163.com>
2022-11-29 15:31:04 +0800

    feat:PDM接收的删除工艺接口增加接口文档备注

--- mes-modules/base/src/main/java/com/richfit/mes/base/entity/param/DeleteProcessParam.java
+++ mes-modules/base/src/main/java/com/richfit/mes/base/entity/param/DeleteProcessParam.java
@@ -1,11 +1,14 @@
 package com.richfit.mes.base.entity.param;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
 public class DeleteProcessParam {
+    @ApiModelProperty(value = "工序id", dataType = "String")
     private List<String> drawIdGroup;
+    @ApiModelProperty(value = "工厂代码", dataType = "String")
     private String dataGroup;
 }
