e8b4d93effacb045ae15ba19654721cba05fb530
luzhiqiang <281747606@.com>
2023-01-05 08:49:51 +0800

    feat:热工预装炉流程

--- mes-modules/produce/src/main/java/com/richfit/mes/produce/controller/heat/HeatTrackAssignController.java
+++ mes-modules/produce/src/main/java/com/richfit/mes/produce/controller/heat/HeatTrackAssignController.java
@@ -4,7 +4,9 @@ import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.richfit.mes.common.core.api.CommonResult;
 import com.richfit.mes.common.core.base.BaseController;
 import com.richfit.mes.common.model.produce.Assign;
+import com.richfit.mes.common.model.produce.PrechargeFurnace;
 import com.richfit.mes.produce.entity.ForDispatchingDto;
+import com.richfit.mes.produce.service.PrechargeFurnaceService;
 import com.richfit.mes.produce.service.heat.HeatTrackAssignService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -16,6 +18,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.text.ParseException;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 /**
  * @author zhiqiang.lu
@@ -30,9 +35,27 @@ public class HeatTrackAssignController extends BaseController {
     @Autowired
     private HeatTrackAssignService heatTrackAssignService;
 
+    @Autowired
+    private PrechargeFurnaceService prechargeFurnaceService;
+
+
     @ApiOperation(value = "未报工查询")
     @PostMapping("/queryNotAtWork")
     public CommonResult<IPage<Assign>> queryNotAtWork(@RequestBody ForDispatchingDto dispatchingDto) throws ParseException {
         return CommonResult.success(heatTrackAssignService.queryNotAtWork(dispatchingDto));
+    }
+
+    @ApiOperation(value = "装炉")
+    @PostMapping("/save")
+    public CommonResult<List<Assign>> save(@RequestBody List<Assign> assignList) throws ParseException {
+        Set<String> optNames = new HashSet();
+        PrechargeFurnace prechargeFurnace = new PrechargeFurnace();
+        prechargeFurnace.setTempWork("");
+        for (Assign assign : assignList) {
+            optNames.add(assign.getOptName());
+        }
+        prechargeFurnace.setOptName(optNames.toString());
+        prechargeFurnaceService.save(prechargeFurnace);
+        return CommonResult.success(assignList);
     }
 }
---
+++ mes-modules/produce/src/main/java/com/richfit/mes/produce/service/PrechargeFurnaceService.java
@@ -0,0 +1,11 @@
+package com.richfit.mes.produce.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.richfit.mes.common.model.produce.PrechargeFurnace;
+
+/**
+ * @Author: zhiqiang.lu
+ * @Date: 2023.1.4
+ */
+public interface PrechargeFurnaceService extends IService<PrechargeFurnace> {
+}
