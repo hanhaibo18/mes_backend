3f38e5d4b75e776950c729350f69243062ad5ea9
luzhiqiang <281747606@.com>
2023-03-31 17:15:13 +0800

    feat:日志添加备注查询

--- mes-modules/produce/src/main/java/com/richfit/mes/produce/controller/ActionController.java
+++ mes-modules/produce/src/main/java/com/richfit/mes/produce/controller/ActionController.java
@@ -1,5 +1,6 @@
 package com.richfit.mes.produce.controller;
 
+import cn.hutool.core.util.StrUtil;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.mysql.cj.util.StringUtils;
@@ -31,7 +32,7 @@ public class ActionController {
      */
     @ApiOperation(value = "查询操作信息", notes = "根据查询条件返回操作信息")
     @GetMapping("/action")
-    public CommonResult queryByCondition(String startTime, String endTime, String user, String actionType, String actionItem, int page, int limit) throws GlobalException {
+    public CommonResult queryByCondition(String startTime, String endTime, String user, String actionType, String actionItem, String remark, String branchId, int page, int limit) throws GlobalException {
 
         QueryWrapper<Action> queryWrapper = new QueryWrapper<Action>();
         if (!StringUtils.isNullOrEmpty(startTime)) {
@@ -49,11 +50,10 @@ public class ActionController {
         if (!StringUtils.isNullOrEmpty(actionItem)) {
             queryWrapper.eq("action_item", actionItem);
         }
-//        queryWrapper.eq("branch_id", SecurityUtils.getCurrentUser().getOrgId());
+        queryWrapper.like(StrUtil.isNotBlank(remark), "remark", remark);
+        queryWrapper.eq(StrUtil.isNotBlank(branchId), "branch_id", branchId);
         queryWrapper.eq("tenant_id", SecurityUtils.getCurrentUser().getTenantId());
-
         queryWrapper.orderByDesc("action_time");
-
         return CommonResult.success(actionService.page(new Page<Action>(page, limit), queryWrapper));
 
     }
