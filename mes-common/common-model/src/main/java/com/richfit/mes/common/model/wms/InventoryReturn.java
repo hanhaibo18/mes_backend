60c33e150480f0c965159424b91b24c9136f7402
long <1731529193@qq.com>
2023-04-07 09:57:18 +0800

    feat: 查询wms库存返回信息

--- mes-common/common-model/src/main/java/com/richfit/mes/common/model/wms/InventoryReturn.java
+++ mes-common/common-model/src/main/java/com/richfit/mes/common/model/wms/InventoryReturn.java
@@ -4,41 +4,79 @@ import com.baomidou.mybatisplus.annotation.TableField;
 import lombok.Data;
 
 /**
- * MES实时查询WMS库存 返回类型
+ * MES实时查询WMS库存 返回对象
  */
 @Data
 public class InventoryReturn {
 
+    /**
+     * 库存类型
+     */
     private String invType;
 
-
+    /**
+     * 库存数量
+     */
     private double quantity;
 
-
+    /**
+     * 物料描述
+     */
     private String materialDesc;
 
-
+    /**
+     * 产品编码
+     */
     private String productNum;
 
-
+    /**
+     * 计量单位
+     */
     private String unit;
 
+    /**
+     * 生产日期
+     */
     private String productionDate;
 
+    /**
+     * 工厂
+     */
     private String workCode;
 
+    /**
+     * 物料组
+     */
     private String materialGroup;
 
+    /**
+     * 物料编码
+     */
     private String materialNum;
 
+    /**
+     * 有效期
+     */
     private String validityDate;
 
+    /**
+     * 工作号
+     */
     private String jobNo;
 
+    /**
+     * 仓位
+     */
     private String location;
 
+    /**
+     * 库存id
+     */
     private String id;
 
+    /**
+     * 库房
+     */
     private String invCode;
 
     //图号
