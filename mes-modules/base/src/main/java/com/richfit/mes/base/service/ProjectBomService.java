6c62e843f43fdd45eae08a94f4355ed293987e4b
luzhiqiang <281747606@.com>
2023-04-11 09:43:04 +0800

    fix:项目BOM页面增加工作号查询。

--- mes-modules/base/src/main/java/com/richfit/mes/base/service/ProjectBomService.java
+++ mes-modules/base/src/main/java/com/richfit/mes/base/service/ProjectBomService.java
@@ -55,7 +55,7 @@ public interface ProjectBomService extends IService<ProjectBom> {
      * @Date: 2022/5/31 0:38
      * @return: IPage<ProjectBom>
      **/
-    IPage<ProjectBom> getProjectBomPage(String drawingNo, String projectName, String prodDesc, String state, String tenantId, String branchCode, String order, String orderCol, String publishState, int page, int limit);
+    IPage<ProjectBom> getProjectBomPage(String drawingNo, String projectName, String prodDesc, String workPlanNo, String state, String tenantId, String branchCode, String order, String orderCol, String publishState, int page, int limit);
 
     /**
      * 功能描述:
