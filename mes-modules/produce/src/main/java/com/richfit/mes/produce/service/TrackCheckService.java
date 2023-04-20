6c7b3499f1dc5cf1e23f477920f873bb69c5acb3
houxinyu <houxinyu@cnpc.com.cn>
2023-04-20 16:34:42 +0800

    ## fix: 修改报工/质检/调度逻辑

--- mes-modules/produce/src/main/java/com/richfit/mes/produce/service/TrackCheckService.java
+++ mes-modules/produce/src/main/java/com/richfit/mes/produce/service/TrackCheckService.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.richfit.mes.common.core.api.CommonResult;
 import com.richfit.mes.common.model.produce.TrackCheck;
 import com.richfit.mes.common.model.produce.TrackItem;
 import com.richfit.mes.produce.entity.CountDto;
@@ -58,4 +59,50 @@ public interface TrackCheckService extends IService<TrackCheck> {
      * @return: Boolean
      **/
     Boolean countQueryRules(String rulesId);
+
+    /**
+     * 功能描述: 质检审核分页查询
+     *
+     * @param page
+     * @param limit
+     * @param isExistQualityCheck
+     * @param isScheduleComplete
+     * @param startTime
+     * @param endTime
+     * @param trackNo
+     * @param productNo
+     * @param tenantId
+     * @param isRecheck
+     * @param drawingNo
+     * @param order
+     * @param orderCol
+     * @Author: xinYu.hou
+     * @Date: 2023/4/19 18:15
+     * @return: IPage<TrackItem>
+     **/
+    CommonResult<IPage<TrackItem>> queryQualityPage(int page, int limit, String isExistQualityCheck, String isScheduleComplete, String startTime, String endTime, String trackNo, String productNo, String tenantId, Boolean isRecheck, String drawingNo, String order, String orderCol);
+
+
+    /**
+     * 功能描述: 调度审核分页查询
+     *
+     * @param page
+     * @param limit
+     * @param isExistScheduleCheck
+     * @param isScheduleComplete
+     * @param startTime
+     * @param endTime
+     * @param trackNo
+     * @param productNo
+     * @param branchCode
+     * @param tenantId
+     * @param drawingNo
+     * @param order
+     * @param orderCol
+     * @Author: xinYu.hou
+     * @Date: 2023/4/19 18:15
+     * @return: IPage<TrackCheck>
+     **/
+    CommonResult<IPage<TrackItem>> queryDispatchPage(int page, int limit, String isExistScheduleCheck, String isScheduleComplete, String startTime, String endTime, String trackNo, String productNo, String branchCode, String tenantId, String drawingNo, String order, String orderCol);
+
 }
