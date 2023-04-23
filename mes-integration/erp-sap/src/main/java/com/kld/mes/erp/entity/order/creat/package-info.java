a33448f3ae8547cca9ed33da7bb9de3536634a77
luzhiqiang <281747606@.com>
2023-04-23 15:32:05 +0800

    feat:订单创建接口

--- mes-integration/erp-sap/src/main/java/com/kld/mes/erp/entity/order/creat/package-info.java
+++ mes-integration/erp-sap/src/main/java/com/kld/mes/erp/entity/order/creat/package-info.java
@@ -1,2 +1,2 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "urn:sap-com:document:sap:soap:functions:mc-style")
+@javax.xml.bind.annotation.XmlSchema(namespace = "urn:sap-com:document:sap:rfc:functions.response", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
 package com.kld.mes.erp.entity.order.creat;
