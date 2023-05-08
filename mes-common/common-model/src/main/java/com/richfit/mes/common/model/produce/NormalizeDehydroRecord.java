de2e311dd09086c0c89143e118269d4a887d8d03
hujia <18911024287@163.com>
2023-05-08 16:29:51 +0800

    feat:增加正火去氢记录相关接口

--- mes-common/common-model/src/main/java/com/richfit/mes/common/model/produce/NormalizeDehydroRecord.java
+++ mes-common/common-model/src/main/java/com/richfit/mes/common/model/produce/NormalizeDehydroRecord.java
@@ -1,8 +1,10 @@
 package com.richfit.mes.common.model.produce;
 
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.richfit.mes.common.core.base.BaseEntity;
 import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.ApiParam;
 import lombok.Data;
 
 /**
@@ -15,19 +17,10 @@ import lombok.Data;
 @TableName("produce_normalize_dehydro_record")
 public class NormalizeDehydroRecord extends BaseEntity<NormalizeDehydroRecord> {
 
-    @ApiModelProperty(value = "跟单工序id", dataType = "String")
-    private String itemId;
-    @ApiModelProperty(value = "产品名称", dataType = "String")
-    private String productName;
-    @ApiModelProperty(value = "图    号", dataType = "String")
-    private String drawNo;
     @ApiModelProperty(value = "记录编号", dataType = "String")
     private String serialNo;
-    @ApiModelProperty(value = "数量", dataType = "Integer")
-    private Integer number;
     @ApiModelProperty(value = "0 正火  1 去氢", dataType = "Integer")
     private Integer type;
-
     @ApiModelProperty(value = "设备编号", dataType = "String")
     private String equipmentNo;
     @ApiModelProperty(value = "审核状态 0 未通过  1 通过", dataType = "Integer")
@@ -36,5 +29,15 @@ public class NormalizeDehydroRecord extends BaseEntity<NormalizeDehydroRecord> {
     private String attachmentId;
     @ApiModelProperty(value = "预装炉id", dataType = "String")
     private String furnaceId;
+    @ApiModelProperty(value = "问题处理", dataType = "String")
+    private String problemProcessing;
+
+    @ApiModelProperty(value = "开始时间", dataType = "String")
+    @TableField(exist = false)
+    private String startTime;
+    @ApiModelProperty(value = "结束时间", dataType = "String")
+    @TableField(exist = false)
+    private String endTime;
+
 }
 
