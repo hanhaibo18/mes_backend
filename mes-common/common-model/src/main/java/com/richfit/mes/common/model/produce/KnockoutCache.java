9218455f84f2227f1d3ece79cff62d55ad785436
hanhaibo <1667548706@qq.com>
2023-05-09 09:37:01 +0800

    feat：造型/制芯/扣箱/打箱工序报工接口

---
+++ mes-common/common-model/src/main/java/com/richfit/mes/common/model/produce/KnockoutCache.java
@@ -0,0 +1,21 @@
+package com.richfit.mes.common.model.produce;
+
+import java.util.Date;
+
+import com.baomidou.mybatisplus.extension.activerecord.Model;
+import com.richfit.mes.common.core.base.BaseEntity;
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * 扣箱工序报工缓存记录表(ProduceKnockoutCache)表实体类
+ *
+ * @author makejava
+ * @since 2023-05-08 10:06:04
+ */
+@Data
+public class KnockoutCache extends Knockout {
+
+}
+
---
+++ mes-modules/produce/src/main/java/com/richfit/mes/produce/service/KnockoutCacheServiceImpl.java
@@ -0,0 +1,18 @@
+package com.richfit.mes.produce.service;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.richfit.mes.common.model.produce.KnockoutCache;
+import com.richfit.mes.produce.dao.KnockoutCacheMapper;
+import org.springframework.stereotype.Service;
+
+/**
+ * 扣箱工序报工缓存记录表(KnockoutCache)表服务实现类
+ *
+ * @author makejava
+ * @since 2023-05-08 10:19:22
+ */
+@Service
+public class KnockoutCacheServiceImpl extends ServiceImpl<KnockoutCacheMapper, KnockoutCache> implements KnockoutCacheService {
+
+}
+
