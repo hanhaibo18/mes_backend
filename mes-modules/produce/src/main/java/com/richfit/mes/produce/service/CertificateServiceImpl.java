4320905f531cca5777f466d8de08df4efc36f53a
luzhiqiang <281747606@.com>
2023-05-18 18:36:08 +0800

    fix:合格证检验员改为System

--- mes-modules/produce/src/main/java/com/richfit/mes/produce/service/CertificateServiceImpl.java
+++ mes-modules/produce/src/main/java/com/richfit/mes/produce/service/CertificateServiceImpl.java
@@ -90,8 +90,7 @@ public class CertificateServiceImpl extends ServiceImpl<CertificateMapper, Certi
         certificate.setBranchCode(trackHead.getBranchCode());
         certificate.setCertOrigin("0");
         certificate.setCertificateNo(Code.valueOnUpdate("hege_no", trackHead.getTenantId(), trackHead.getBranchCode(), codeRuleService));
-        CommonResult<TenantUserVo> commonResult = systemServiceClient.queryByUserId(user.getUserId());
-        certificate.setCheckName(commonResult.getData().getEmplName());
+        certificate.setCheckName("System");
         certificate.setCheckTime(new Date());
         certificate.setDrawingNo(trackHead.getDrawingNo());
         certificate.setMaterialNo(trackHead.getMaterialNo());
