dd2ef986bf1753f2957e25388eae5304877b7423
luzhiqiang <281747606@.com>
2023-05-26 10:25:43 +0800

    fix:合格证代码整理，添加erp工时推送手动

--- mes-modules/produce/src/main/java/com/richfit/mes/produce/controller/erp/WorkHoursController.java
+++ mes-modules/produce/src/main/java/com/richfit/mes/produce/controller/erp/WorkHoursController.java
@@ -27,6 +27,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 
+import static com.richfit.mes.produce.service.bsns.AbstractCertAdditionalBsns.needScjk;
 import static java.util.stream.Collectors.collectingAndThen;
 import static java.util.stream.Collectors.toCollection;
 
@@ -38,7 +39,7 @@ import static java.util.stream.Collectors.toCollection;
 @Api(value = "合格证管理", tags = {"合格证管理"})
 @RestController
 @RequestMapping("/api/produce/work/hours")
-public class CertificateController {
+public class WorkHoursController {
 
     @Autowired
     private CertificateService certificateService;
@@ -49,10 +50,47 @@ public class CertificateController {
     @Autowired
     public CertAdditionalBsns certAdditionalBsns;
 
+    @ApiOperation(value = "自动推送工时", notes = "自动推送工时")
+    @PostMapping("/push")
+    public CommonResult push(@ApiParam(value = "合格证", required = true) @RequestBody List<Certificate> certificateList) throws Exception {
+        int i = 0;
+        String message = "";
+        for (Certificate certificate : certificateList) {
+            System.out.println("---------------------------------" + i++);
+            if (!Certificate.NEXT_OPT_WORK_BOMCO_SC.equals(certificate.getNextOptWork())) {
+                message += certificate.getCertificateNo() + ":非生产入库合格证不进行工时推送;";
+                //非生产入库合格证不进行工时推送
+                break;
+            }
+            if (Certificate.IS_SENG_WORK_HOUR_1.equals(certificate.getIsSendWorkHour())) {
+                //已经推送过工时的不进行工时推送
+                break;
+            }
+            try {
+                QueryWrapper<TrackHead> queryWrapperTrackHead = new QueryWrapper<>();
+                queryWrapperTrackHead.eq("certificate_no", certificate.getCertificateNo());
+                queryWrapperTrackHead.eq("tenant_id", certificate.getCertificateNo());
+                List<TrackHead> trackHeadList = trackHeadService.list(queryWrapperTrackHead);
+                if (CollectionUtils.isNotEmpty(trackHeadList)) {
+                    List<TrackCertificate> trackCertificates = new ArrayList<>();
+                    for (TrackHead trackHead : trackHeadList) {
+                        TrackCertificate trackCertificate = new TrackCertificate();
+                        trackCertificate.setThId(trackHead.getId());
+                        trackCertificates.add(trackCertificate);
+                    }
+                    certificate.setTrackCertificates(trackCertificates);
+                    certAdditionalBsns.pushWorkHour(certificate);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return CommonResult.success("");
+    }
 
     @ApiOperation(value = "自动推送工时", notes = "自动推送工时")
     @GetMapping("/auto/push")
-    public void pushHours() throws Exception {
+    public void autoPush() throws Exception {
         QueryWrapper<Certificate> queryWrapper = new QueryWrapper<>();
         queryWrapper.ne("is_send_work_hour", "1");
         queryWrapper.eq("next_opt_work", "BOMCO_SC");
