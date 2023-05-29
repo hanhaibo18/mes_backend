b4afae640f1c775c8b85c5bd57f671b09b0bfd4e
luzhiqiang <281747606@.com>
2023-05-29 11:08:03 +0800

    fix:客户要求工作号改模糊查询

--- mes-modules/produce/src/main/java/com/richfit/mes/produce/controller/TrackAssignController.java
+++ mes-modules/produce/src/main/java/com/richfit/mes/produce/controller/TrackAssignController.java
@@ -727,7 +727,7 @@ public class TrackAssignController extends BaseController {
             queryWrapper.like("product_no", productNo);
         }
         if (!StringUtils.isNullOrEmpty(workNo)) {
-            queryWrapper.inSql("a.id", "select id from produce_track_item where track_head_id in (select id from produce_track_head where tenant_id = '" + SecurityUtils.getCurrentUser().getTenantId() + "' and work_no = '" + workNo + "')");
+            queryWrapper.inSql("a.id", "select id from produce_track_item where track_head_id in (select id from produce_track_head where tenant_id = '" + SecurityUtils.getCurrentUser().getTenantId() + "' and work_no like '%" + workNo + "%')");
         }
 
 
