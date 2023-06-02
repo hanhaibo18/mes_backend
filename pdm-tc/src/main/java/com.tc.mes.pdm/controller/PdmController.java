2ba738c2e341355a6ed4700269e903ee1f848ad1
long <1731529193@qq.com>
2023-06-02 14:36:44 +0800

    fix： pdm-tc模板修改

---
+++ pdm-tc/src/main/java/com.tc.mes.pdm/controller/PdmController.java
@@ -0,0 +1,27 @@
+package com.tc.mes.pdm.controller;
+
+import com.richfit.mes.common.core.api.CommonResult;
+import com.tc.mes.pdm.entity.PlmResult;
+import com.tc.mes.pdm.entity.ProductionSchedulingDto;
+import com.tc.mes.pdm.service.ProductToPdmService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@Api("PDM plm-tc接口")
+@RestController
+@RequestMapping("/api/integration/plm")
+public class PdmController {
+
+    @Autowired
+    private ProductToPdmService productToPdmService;
+
+    @ApiOperation(value = "生产排产单同步", notes = "MES生产排产单同步PLM")
+    @PostMapping("/production_scheduling_sync")
+    public CommonResult<PlmResult> ProductionSchedulingSync(@RequestBody ProductionSchedulingDto productionSchedulingDto) throws Exception {
+        return new CommonResult(productToPdmService.ProductionSchedulingSync(productionSchedulingDto));
+    }
+}
