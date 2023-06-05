d188727509bc503e41063bc0e7092a8eb2aba7d3
hanhaibo <1667548706@qq.com>
2023-06-05 10:53:44 +0800

    feat:PDM图纸查询

--- mes-modules/base/src/main/java/com/richfit/mes/base/service/PdmObjectServiceImpl.java
+++ mes-modules/base/src/main/java/com/richfit/mes/base/service/PdmObjectServiceImpl.java
@@ -1,9 +1,16 @@
 package com.richfit.mes.base.service;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.mysql.cj.util.StringUtils;
 import com.richfit.mes.base.dao.PdmObjectMapper;
+import com.richfit.mes.common.model.base.PdmBom;
+import com.richfit.mes.common.model.base.PdmDraw;
 import com.richfit.mes.common.model.base.PdmObject;
+import com.richfit.mes.common.model.base.PdmProcess;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,6 +21,13 @@ import java.util.List;
  */
 @Service
 public class PdmObjectServiceImpl extends ServiceImpl<PdmObjectMapper, PdmObject> implements PdmObjectService {
+
+    @Autowired
+    private PdmBomService pdmBomService;
+    @Autowired
+    private PdmDrawService pdmDrawService;
+    @Autowired
+    private PdmProcessService pdmProcessService;
 
     /**
      * 功能描述: 根据图号和数据分组查询工装
@@ -37,5 +51,45 @@ public class PdmObjectServiceImpl extends ServiceImpl<PdmObjectMapper, PdmObject
         QueryWrapper<PdmObject> queryWrapper = new QueryWrapper<>();
         queryWrapper.eq("op_id", optId);
         return this.list(queryWrapper);
+    }
+
+    @Override
+    public IPage getPdmPage(String drawNo, String type, String branchCode, int page, int limit) {
+        switch (type) {
+            case "draw":
+                QueryWrapper<PdmDraw> drawQueryWrapper = new QueryWrapper<>();
+                drawQueryWrapper.eq("datagroup", branchCode);
+                if (!StringUtils.isNullOrEmpty(drawNo)) {
+                    drawQueryWrapper.eq("item_id", drawNo);
+                }
+                Page<PdmDraw> pdmDrawPage = pdmDrawService.page(new Page<>(page, limit), drawQueryWrapper);
+                for (PdmDraw pdmDraw : pdmDrawPage.getRecords()) {
+                    pdmDraw.setType("图纸");
+                }
+                return pdmDrawPage;
+            case "bom":
+                QueryWrapper<PdmBom> bomQueryWrapper = new QueryWrapper<>();
+                bomQueryWrapper.eq("datagroup",branchCode);
+                if (!StringUtils.isNullOrEmpty(drawNo)){
+                    bomQueryWrapper.eq("id",drawNo);
+                }
+                Page<PdmBom> pdmBomPage = pdmBomService.page(new Page<>(page, limit), bomQueryWrapper);
+                for (PdmBom pdmBom : pdmBomPage.getRecords()) {
+                    pdmBom.setType("BOM");
+                }
+                return pdmBomPage;
+            case "router":
+                QueryWrapper<PdmProcess> processQueryWrapper = new QueryWrapper<>();
+                processQueryWrapper.eq("dataGroup",branchCode);
+                if (!StringUtils.isNullOrEmpty(drawNo)){
+                    processQueryWrapper.eq("draw_no",drawNo);
+                }
+                Page<PdmProcess> pdmProcessPage = pdmProcessService.page(new Page<>(page, limit), processQueryWrapper);
+                for (PdmProcess process : pdmProcessPage.getRecords()) {
+                    process.setType("工艺");
+                }
+                return pdmProcessPage;
+        }
+        return new Page<>();
     }
 }
