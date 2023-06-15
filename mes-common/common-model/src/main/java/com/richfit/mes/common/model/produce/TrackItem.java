2678776ac818122cddbe4dfedb20f89ae76d2a94
houxinyu <houxinyu@cnpc.com.cn>
2023-06-15 15:21:32 +0800

    ## fix: 质检审核列表 增加报工人参数

--- mes-common/common-model/src/main/java/com/richfit/mes/common/model/produce/TrackItem.java
+++ mes-common/common-model/src/main/java/com/richfit/mes/common/model/produce/TrackItem.java
@@ -543,5 +543,6 @@ public class TrackItem extends BaseEntity<TrackItem> {
     @TableField(exist = false)
     private String blankSpecifi;
 
-
+    @TableField(exist = false)
+    private String userName;
 }
