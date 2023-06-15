ea790526965e0d3a7933adec9f222d9d2b0b798d
luzhiqiang <281747606@.com>
2023-06-15 16:18:35 +0800

    fix:erp修改为测试地址

--- mes-gateway/gateway-web/src/main/java/com/richfit/mes/gateway/web/GatewayApplication.java
+++ mes-gateway/gateway-web/src/main/java/com/richfit/mes/gateway/web/GatewayApplication.java
@@ -1,12 +1,21 @@
 package com.richfit.mes.gateway.web;
 
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.deser.std.StdScalarDeserializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.TimeZone;
 
 /**
  * @author sun
@@ -15,9 +24,28 @@ import org.springframework.context.annotation.Bean;
 
 @EnableCircuitBreaker
 @EnableDiscoveryClient
-@SpringBootApplication(scanBasePackages="com.richfit.mes")
+@SpringBootApplication(scanBasePackages = "com.richfit.mes")
 public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
+    }
+
+    /**
+     * 全局返回json格式化
+     */
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer jackson2ObjectMapperBuilderCustomizer() {
+        TimeZone tz = TimeZone.getTimeZone("UTC");
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        df.setTimeZone(tz);
+        return builder -> builder
+                .deserializerByType(String.class, new StdScalarDeserializer<String>(String.class) {
+                    @Override
+                    public String deserialize(JsonParser jsonParser, DeserializationContext ctx)
+                            throws IOException {
+                        // 去除前后空格
+                        return org.springframework.util.StringUtils.trimWhitespace(jsonParser.getValueAsString());
+                    }
+                }).dateFormat(df);
     }
 }
