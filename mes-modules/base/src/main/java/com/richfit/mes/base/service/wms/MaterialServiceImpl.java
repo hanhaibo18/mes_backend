031753238b2a6bd2e4c176128d14392bc6eeac45
luzhiqiang <281747606@.com>
2023-06-15 10:59:35 +0800

    fix:修改物料同步

---
+++ mes-modules/base/src/main/java/com/richfit/mes/base/service/wms/MaterialServiceImpl.java
@@ -0,0 +1,62 @@
+package com.richfit.mes.base.service.wms;
+
+import com.baomidou.mybatisplus.core.toolkit.CollectionUtils;
+import com.richfit.mes.base.provider.WmsServiceClient;
+import com.richfit.mes.base.service.ProductService;
+import com.richfit.mes.common.core.api.CommonResult;
+import com.richfit.mes.common.core.api.ResultCode;
+import com.richfit.mes.common.model.base.Product;
+import com.richfit.mes.common.model.wms.MaterialBasis;
+import com.richfit.mes.common.security.util.SecurityUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 功能描述:物料管理
+ *
+ * @Author: zhiqiang.lu
+ * @Date: 2023/05/26 16:27
+ **/
+@Service
+@Transactional(rollbackFor = Exception.class)
+public class MaterialServiceImpl implements MaterialService {
+
+    @Autowired
+    private ProductService productService;
+
+    @Autowired
+    public WmsServiceClient wmsServiceClient;
+
+    @Override
+    public void sync(List<Product> products) {
+        this.sync(products, SecurityUtils.getCurrentUser().getTenantErpCode());
+    }
+
+    @Override
+    public void sync(List<Product> products, String erpCode) {
+        if (CollectionUtils.isEmpty(products)) {
+            return;
+        }
+        List<MaterialBasis> materialBasisList = new ArrayList<>(products.size());
+        for (Product product : products) {
+            MaterialBasis materialBasis = new MaterialBasis(product);
+            materialBasisList.add(materialBasis);
+        }
+        // 同步到wms中
+        CommonResult commonResult = wmsServiceClient.materialBasis(materialBasisList);
+        if (commonResult.getStatus() == ResultCode.SUCCESS.getCode()) {
+            for (Product product : products) {
+                product.setSynchronousRegime(1);
+            }
+        } else {
+            for (Product product : products) {
+                product.setSynchronousRegime(2);
+            }
+        }
+        productService.updateBatchById(products);
+    }
+}
