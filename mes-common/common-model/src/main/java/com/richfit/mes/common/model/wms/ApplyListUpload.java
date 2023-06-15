eb714e8c9c993d0fe990671575ae6fc7e1f78edb
long <1731529193@qq.com>
2023-06-15 16:02:44 +0800

    fix: 合格证生产入库

--- mes-common/common-model/src/main/java/com/richfit/mes/common/model/wms/ApplyListUpload.java
+++ mes-common/common-model/src/main/java/com/richfit/mes/common/model/wms/ApplyListUpload.java
@@ -125,9 +125,9 @@ public class ApplyListUpload implements Serializable {
         this.applyNum = UUID.randomUUID().toString().replaceAll("-", "");
         if (certificate.getTenantId().equals(TENANT_ID)) {
             // 单据类型
-            this.transType = NORMAL_TYPE;
-        } else {
             this.transType = AUTO_TYPE;
+        } else {
+            this.transType = NORMAL_TYPE;
         }
         // 工厂
         this.workCode = tenantErpCode;
@@ -149,142 +149,7 @@ public class ApplyListUpload implements Serializable {
         this.createTime = certificate.getCreateTime();
         // 明细列表
         this.lineList = new ArrayList<>(1);
-
-        ApplyLineList applyLineList = new ApplyLineList();
-        // 申请单id
-        applyLineList.setApplyId(UUID.randomUUID().toString().replaceAll("-", ""));
-        // id
-        applyLineList.setId(this.id);
-        // 行项目
-        applyLineList.setLineNum(1);
-        // 图号
-        applyLineList.setDrawingNo(certificate.getDrawingNo());
-        // 物料号
-        applyLineList.setMaterialNum(certificate.getMaterialNo());
-        // 物料名称
-        applyLineList.setMaterialDesc(certificate.getMaterialName());
-        // 单位
-        applyLineList.setUnit(product.getUnit());
-        // 产品编号
-        applyLineList.setQuantity(certificate.getNumber());
-        // 物料类型
-        if (StringUtils.isNotEmpty(product.getMaterialType())) {
-            applyLineList.setMaterialType(MaterialTypeEnum.getMessage(product.getMaterialType()));
-        }
-        // 关键件
-        if (StringUtils.isNotEmpty(product.getIsKeyPart())) {
-            applyLineList.setCrucialFlag(MessageEnum.getMessage(product.getIsKeyPart()));
-        }
-        // 跟踪方式
-        if (StringUtils.isNotEmpty(product.getTrackType())) {
-            applyLineList.setTrackingMode(TrackTypeEnum.getMessage(product.getTrackType()));
-        }
-        // 物料名称
-        applyLineList.setMaterialDesc(product.getProductName());
-        List<ApplyLineProductList> list = new ArrayList<>(trackFlows.size());
-        for (TrackFlow trackFlow : trackFlows) {
-            ApplyLineProductList applyLineProductList = new ApplyLineProductList();
-            // 申请单行id
-            applyLineProductList.setApplyLineId(certificate.getId());
-            // 产品编号
-            applyLineProductList.setProductNum(trackFlow.getProductNo());
-            // 数量
-            applyLineProductList.setQuantity(trackFlow.getNumber());
-            list.add(applyLineProductList);
-        }
-        applyLineList.setLineList(list);
+        ApplyLineList applyLineList = new ApplyLineList(certificate,product,trackFlows);
         lineList.add(applyLineList);
-    }
-
-    /**
-     * @param certificate   合格证
-     * @param products      物料list
-     * @param trackFlows    跟单分流list
-     * @param tenantErpCode 租户erp标识
-     */
-    public ApplyListUpload(Certificate certificate, List<Product> products, List<TrackFlow> trackFlows, String tenantErpCode) {
-        this.id = certificate.getId();
-        // MES申请单号
-        this.applyNum = UUID.randomUUID().toString().replaceAll("-", "");
-        if (certificate.getTenantId().equals(TENANT_ID)) {
-            // 单据类型
-            this.transType = AUTO_TYPE;
-        } else {
-            this.transType = NORMAL_TYPE;
-        }
-        // 工厂
-        this.workCode = tenantErpCode;
-        // 车间
-        this.workshop = certificate.getBranchCode();
-        // 库存地点 待定
-        this.invCode = null;
-        // 工作号
-        this.jobNo = certificate.getWorkNo();
-        // 生产订单
-        this.prodNum = certificate.getProductionOrder();
-        // 合格证
-        this.certificate = certificate.getCertificateNo();
-        // 炉批号
-        this.batchNo = certificate.getBatchNo();
-        // 创建人
-        this.createBy = certificate.getCreateBy();
-        // 创建日期
-        this.createTime = certificate.getCreateTime();
-        // 明细列表
-        this.lineList = new ArrayList<>(products.size());
-        int i = 0;
-        for (Product product : products) {
-            //TODO  这个判断有社么意义吗？
-            if (this.id.equals(certificate.getId())) {
-                ApplyLineList applyLineList = new ApplyLineList();
-                // 申请单id
-                applyLineList.setApplyId(UUID.randomUUID().toString().replaceAll("-", ""));
-                // id
-                applyLineList.setId(this.id);
-                // 行项目
-                applyLineList.setLineNum(i++);
-                // 图号
-                applyLineList.setDrawingNo(certificate.getDrawingNo());
-                // 物料号
-                applyLineList.setMaterialNum(certificate.getMaterialNo());
-                // 物料名称
-                applyLineList.setMaterialDesc(certificate.getMaterialName());
-                // 单位
-                applyLineList.setUnit(product.getUnit());
-                // 产品编号
-                applyLineList.setQuantity(certificate.getNumber());
-                // 物料类型
-                if (StringUtils.isNotEmpty(product.getMaterialType())) {
-                    applyLineList.setMaterialType(MaterialTypeEnum.getMessage(product.getMaterialType()));
-                }
-                // 关键件
-                if (StringUtils.isNotEmpty(product.getIsKeyPart())) {
-                    applyLineList.setCrucialFlag(MessageEnum.getMessage(product.getIsKeyPart()));
-                }
-                // 跟踪方式
-                if (StringUtils.isNotEmpty(product.getTrackType())) {
-                    applyLineList.setTrackingMode(TrackTypeEnum.getMessage(product.getTrackType()));
-                }
-                // 物料名称
-                applyLineList.setMaterialDesc(product.getProductName());
-                List<ApplyLineProductList> list = new ArrayList<>(trackFlows.size());
-                for (TrackFlow trackFlow : trackFlows) {
-                    //TODO  这个判断有社么意义吗？
-                    if (applyLineList.getId().equals(certificate.getId())) {
-                        ApplyLineProductList applyLineProductList = new ApplyLineProductList();
-                        // 申请单行id
-                        applyLineProductList.setApplyLineId(certificate.getId());
-                        // 产品编号
-                        applyLineProductList.setProductNum(trackFlow.getProductNo());
-                        // 数量
-                        applyLineProductList.setQuantity(trackFlow.getNumber());
-                        list.add(applyLineProductList);
-                    }
-                }
-                applyLineList.setLineList(list);
-                lineList.add(applyLineList);
-            }
-
-        }
     }
 }
