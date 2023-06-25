28e8be86c728902d9cb06f8a762714caf5ca1679
long <1731529193@qq.com>
2023-06-25 18:15:40 +0800

    fix:生产排产单同步

--- mes-integration/plm-tc/src/main/java/com/tc/mes/plm/controller/ProductToPdmController.java
+++ mes-integration/plm-tc/src/main/java/com/tc/mes/plm/controller/ProductToPdmController.java
@@ -1,15 +1,13 @@
 package com.tc.mes.plm.controller;
 
-import com.tc.mes.plm.entity.PdmResult;
-import com.tc.mes.plm.entity.vo.ProduceNoticeVo;
+import com.richfit.mes.common.model.produce.Notice;
+import com.tc.mes.plm.common.Result;
 import com.tc.mes.plm.service.impl.ProductToPdmServiceImpl;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @Slf4j
 @Api("PDM plm-tc接口")
@@ -22,13 +20,13 @@ public class ProductToPdmController {
 
     @ApiOperation(value = "生产排产单同步", notes = "MES生产排产单同步PLM")
     @PostMapping("/production_scheduling_sync")
-    public PdmResult productionSchedulingSync(@RequestBody List<ProduceNoticeVo> produceNoticeDtoList) {
-        return productToPdmService.productionSchedulingSync(produceNoticeDtoList);
+    public Result productionSchedulingSync(@RequestBody Notice notice) {
+        return productToPdmService.productionSchedulingSync(notice);
     }
 
     @ApiOperation(value = "用户登录plm", notes = "用户登录plm")
     @PostMapping("/login")
-    public PdmResult login() {
+    public Result login() {
         return productToPdmService.login();
     }
 
---
+++ mes-integration/plm-tc/src/main/java/com/tc/mes/plm/mapper/NoticeTenantMapper.java
@@ -0,0 +1,19 @@
+package com.tc.mes.plm.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.tc.mes.plm.entity.domain.NoticeTenant;
+
+
+/**
+* @author llh
+* @description 针对表【notice_tenant】的数据库操作Mapper
+* @createDate 2023-06-25 16:52:52
+* @Entity generator.domain.NoticeTenant
+*/
+public interface NoticeTenantMapper extends BaseMapper<NoticeTenant> {
+
+}
+
+
+
+
