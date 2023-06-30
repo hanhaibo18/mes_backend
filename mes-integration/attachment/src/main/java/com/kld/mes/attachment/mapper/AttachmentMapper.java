eb362be215bd84273fe74603716d0a36296ee71f
luzhiqiang <281747606@.com>
2023-06-30 17:16:51 +0800

    fix:规范接口名称plm

--- mes-integration/attachment/src/main/java/com/kld/mes/attachment/mapper/AttachmentMapper.java
+++
@@ -1,8 +0,0 @@
-package com.kld.mes.attachment.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.kld.mes.attachment.entity.Attachment;
-
-public interface AttachmentMapper extends BaseMapper<Attachment> {
-
-}
--- mes-integration/erp-sap/src/main/java/com/kld/mes/erp/entity/order/select/package-info.java
+++
@@ -1,7 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(
-        namespace = "urn:sap-com:document:sap:rfc:functions.response",
-        elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED
-)
-
-package com.kld.mes.erp.entity.order.select;
-
