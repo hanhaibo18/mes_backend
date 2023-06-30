e36ebb7ba870f436a8c2c640e73289486d9fff8b
luzhiqiang <281747606@.com>
2023-06-30 16:29:17 +0800

    feat：合并分支市场营销

---
+++ mes-integration/marketing/src/main/java/com/pdm/mes/schedule/service/NoticeAttachmentService.java
@@ -0,0 +1,8 @@
+package com.pdm.mes.schedule.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.pdm.mes.schedule.entity.domain.NoticeAttachment;
+
+public interface NoticeAttachmentService extends IService<NoticeAttachment> {
+
+}
